1e3c47cff7ebaf4d6da46124cbf5746662bb8304
cneumann <cneumann@5bac5841-c719-aa4e-b3fe-cce5062f897a>
2010-10-23 22:06:48 +0000

    dependencies

--- web/src/org/openmrs/module/pihmalawi/web/controller/PihReportFormController.java
+++ web/src/org/openmrs/module/pihmalawi/web/controller/PihReportFormController.java
@@ -1,13 +1,35 @@
 package org.openmrs.module.pihmalawi.web.controller;
 
 import org.openmrs.module.pihmalawi.reporting.Helper;
+import org.openmrs.module.pihmalawi.reporting.SetupArtMissedAppointment;
 import org.openmrs.module.pihmalawi.reporting.SetupArtWeeklyVisit;
 import org.openmrs.module.pihmalawi.reporting.SetupHivWeeklyOutcome;
+import org.openmrs.module.pihmalawi.reporting.SetupPreArtMissedAppointment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PihReportFormController {
+	
+	@RequestMapping("/module/pihmalawi/remove_partmissedappointment.form")
+	public void removePreArtMissedAppointment() {
+		new SetupPreArtMissedAppointment(new Helper()).deleteReportElements();
+	}
+	
+	@RequestMapping("/module/pihmalawi/register_partmissedappointment.form")
+	public void registerPreArtMissedAppointment() throws Exception {
+		new SetupPreArtMissedAppointment(new Helper()).setup(false);
+	}
+	
+	@RequestMapping("/module/pihmalawi/remove_artmissedappointment.form")
+	public void removeArtMissedAppointment() {
+		new SetupArtMissedAppointment(new Helper()).deleteReportElements();
+	}
+	
+	@RequestMapping("/module/pihmalawi/register_artmissedappointment.form")
+	public void registerArtMissedAppointment() throws Exception {
+		new SetupArtMissedAppointment(new Helper()).setup(false);
+	}
 	
 	@RequestMapping("/module/pihmalawi/remove_artweeklyvisit.form")
 	public void removeArtWeeklyVisit() {
