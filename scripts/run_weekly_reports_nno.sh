984e52202266f27b4ad5b6fb021f77e54368f754
cneumann <cneumann@5bac5841-c719-aa4e-b3fe-cce5062f897a>
2011-01-17 10:38:13 +0000

    mail added

--- scripts/run_weekly_reports_nno.sh
+++ scripts/run_weekly_reports_nno.sh
@@ -5,8 +5,12 @@ MAIL=cneumann@pih.org
 TODAY=`date +%Y%m%d`
 
 NOW=`date +%d`%2F`date +%m`%2F`date +%Y`
+
+# use this on (older) Ubuntu and RedHat systems
 ONE_WEEK_AGO=`date --date "6 Days ago" +%d`%2F`date --date "6 Days ago" +%m`%2F`date --date "6 Days ago" +%Y`
 #ONE_MONTH_AGO=`date -v-1m -v+1d +%d`%2F`date -v-1m -v+1d +%m`%2F`date -v-1m -v+1d +%Y`
+
+# use this at least on MacOS
 #ONE_WEEK_AGO=`date -v-6d +%d`%2F`date -v-6d +%m`%2F`date -v-6d +%Y`
 #ONE_MONTH_AGO=`date -v-1m -v+1d +%d`%2F`date -v-1m -v+1d +%m`%2F`date -v-1m -v+1d +%Y`
 
@@ -27,7 +31,7 @@ ONE_WEEK_AGO=`date --date "6 Days ago" +%d`%2F`date --date "6 Days ago" +%m`%2F`
   org.openmrs.module.reporting.report.renderer.ExcelTemplateRenderer \
   "" \
   HIV_Weekly_Outcome-`echo $TODAY`.xls
-echo "" | mailx -a ART_Missed_Appointment-20110117.xls -s "emr: Upper Neno ART Missed Appointments 20110117" christian.neumann@gmx.de
+echo "" | mailx -a HIV_Weekly_Outcome-`echo $TODAY`.xls -s "emr: Upper Neno HIV Weekly Outcome $TODAY" "$MAIL"
 
 # ART Missed Appointment
 ./run_report.sh \
@@ -37,6 +41,7 @@ echo "" | mailx -a ART_Missed_Appointment-20110117.xls -s "emr: Upper Neno ART M
   org.openmrs.module.reporting.report.renderer.ExcelTemplateRenderer \
   "" \
   ART_Missed_Appointment-`echo $TODAY`.xls
+echo "" | mailx -a ART_Missed_Appointment-`echo $TODAY`.xls -s "emr: Upper Neno ART Missed Appointment Overview $TODAY" "$MAIL"
 
 ./run_report.sh \
   "ART Missed Appointment_" \
@@ -45,6 +50,7 @@ echo "" | mailx -a ART_Missed_Appointment-20110117.xls -s "emr: Upper Neno ART M
   org.openmrs.module.reporting.report.renderer.CohortDetailReportRenderer \
   "html" \
   ART_Missed_Appointment_between_2_and_3_weeks-`echo $TODAY`.html
+echo "" | mailx -a ART_Missed_Appointment_between_2_and_3_weeks-`echo $TODAY`.html -s "emr: Upper Neno ART Missed Appointment Breakdown >=2 <3 weeks $TODAY" "$MAIL"
 
 ./run_report.sh \
   "ART Missed Appointment_" \
@@ -53,6 +59,7 @@ echo "" | mailx -a ART_Missed_Appointment-20110117.xls -s "emr: Upper Neno ART M
   org.openmrs.module.reporting.report.renderer.CohortDetailReportRenderer \
   "html" \
   ART_Missed_Appointment_more_than_3_weeks-`echo $TODAY`.html
+echo "" | mailx -a ART_Missed_Appointment_more_than_3_weeks-`echo $TODAY`.html -s "emr: Upper Neno ART Missed Appointment Breakdown >=3 weeks $TODAY" "$MAIL"
 
 # Weekly Encounter by Location
 ./run_report.sh \
@@ -62,6 +69,7 @@ echo "" | mailx -a ART_Missed_Appointment-20110117.xls -s "emr: Upper Neno ART M
   org.openmrs.module.reporting.report.renderer.ExcelTemplateRenderer \
   "" \
   Weekly_Encounter_By_Location-`echo $TODAY`.xls
+echo "" | mailx -a Weekly_Encounter_By_Location-`echo $TODAY`.xls -s "emr: Upper Neno Weekly Encounter by Location $TODAY" "$MAIL"
 
 # Weekly Encounter by User
 ./run_report.sh \
@@ -71,3 +79,5 @@ echo "" | mailx -a ART_Missed_Appointment-20110117.xls -s "emr: Upper Neno ART M
   org.openmrs.module.reporting.report.renderer.ExcelTemplateRenderer \
   "" \
   Weekly_Encounter_By_User-`echo $TODAY`.xls
+echo "" | mailx -a Weekly_Encounter_By_User-`echo $TODAY`.xls -s "emr: Upper Neno Weekly Encounter by User $TODAY" "$MAIL"
+  
