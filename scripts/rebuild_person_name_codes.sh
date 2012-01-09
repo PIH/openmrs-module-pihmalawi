4d6852312cb36f12c7666e72edd0261d3b4a5fd3
cneumann <cneumann@5bac5841-c719-aa4e-b3fe-cce5062f897a>
2012-01-09 20:01:43 +0000

    added logs

--- scripts/rebuild_person_name_codes.sh
+++ scripts/rebuild_person_name_codes.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-home/emradmin/bart2/script/console <<EOF
+date >>/home/emradmin/rebuid.log
+/home/emradmin/bart2/script/console <<EOF
 PersonNameCode.rebuild_person_name_codes
 exit
 EOF
+date >>/home/emradmin/rebuid.log
+echo "DONE" >>/home/emradmin/rebuid.log
 
