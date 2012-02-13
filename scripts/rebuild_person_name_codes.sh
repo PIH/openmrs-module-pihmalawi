b146fb728af929e4ddfcabc023210e248b0149e5
cneumann <cneumann@5bac5841-c719-aa4e-b3fe-cce5062f897a>
2012-02-13 17:05:25 +0000

    timed

--- scripts/rebuild_person_name_codes.sh
+++ scripts/rebuild_person_name_codes.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+START=`date +%Y%m%d-%H%M`
 date >/home/emradmin/rebuild_person_name_codes.log
 /home/emradmin/bart2/script/console <<EOF
 PersonNameCode.rebuild_person_name_codes
@@ -6,10 +7,11 @@ exit
 EOF
 date >>/home/emradmin/rebuild_person_name_codes.log
 echo "DONE" >>/home/emradmin/rebuild_person_name_codes.log
+STOP=`date +%Y%m%d-%H%M`
 
 MAIL=apzu-emr@apzu.pih.org
 PATH=$PATH:/bin:/usr/bin:/home/emradmin/pihmalawi/scripts
 
 TODAY=`date +%Y%m%d`
 
-echo "" | mailx -s "emr: Upper Neno rebuild_person_name_codes $TODAY" "$MAIL"
+echo "Process run from $START to $STOP" | mailx -s "emr: Upper Neno rebuild_person_name_codes $TODAY" "$MAIL"
