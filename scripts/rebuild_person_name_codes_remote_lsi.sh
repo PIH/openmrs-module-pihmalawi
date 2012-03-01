289ebb43108f126c668623a0f4867e0f40aacfea
cneumann <cneumann@5bac5841-c719-aa4e-b3fe-cce5062f897a>
2012-03-01 19:56:46 +0000

    nix

--- scripts/rebuild_person_name_codes_remote_lsi.sh
+++ scripts/rebuild_person_name_codes_remote_lsi.sh
@@ -9,11 +9,13 @@ scp -P 8999 emradmin@195.200.93.58:/tmp/pn_lsi.sql.bz2 /tmp
 bunzip2 -f /tmp/pn_lsi.sql.bz2
 mysqldump -u openmrs -ppa55ionFruit openmrs person_name person_name_code person patient >/tmp/pn_nno.sql
 mysql -u openmrs -ppa55ionFruit openmrs < /tmp/pn_lsi.sql
-/home/emradmin/pihmalawi/scripts/rebuild_person_name_codes.sh 
+/home/emradmin/pihmalawi/scripts/rebuild_person_name_codes.sh >/tmp/$0_lsi.log
 mysqldump -u openmrs -ppa55ionFruit openmrs person_name person_name_code | bzip2 > /tmp/pn_lsi.sql.bz2
 mysql -u openmrs -ppa55ionFruit openmrs < /tmp/pn_nno.sql
-/home/emradmin/pihmalawi/scripts/rebuild_person_name_codes.sh 
 
 scp -P 8999  /tmp/pn_lsi.sql.bz2 emradmin@195.200.93.58:/tmp
 ssh -p 8999 emradmin@195.200.93.58 "bunzip2 -f /tmp/pn_lsi.sql.bz2"
 ssh -p 8999 emradmin@195.200.93.58 "mysql -u openmrs -ppa55ionFruit openmrs < /tmp/pn_lsi.sql"
+
+/home/emradmin/pihmalawi/scripts/rebuild_person_name_codes.sh >/tmp/$0_nno.log
+
