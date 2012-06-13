ffed419c9d97f4ea342530b851614e760b1e7f76
cneumann <cneumann@5bac5841-c719-aa4e-b3fe-cce5062f897a>
2012-06-13 12:35:02 +0000

    added diagnosis to ccc missed appt

--- src/org/openmrs/module/pihmalawi/reports/renderer/ChronicCareMissedAppointmentBreakdownRenderer.java
+++ src/org/openmrs/module/pihmalawi/reports/renderer/ChronicCareMissedAppointmentBreakdownRenderer.java
@@ -2,10 +2,14 @@ package org.openmrs.module.pihmalawi.reports.renderer;
 
 import java.util.Arrays;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 
 import org.openmrs.Location;
+import org.openmrs.Obs;
 import org.openmrs.Patient;
 import org.openmrs.PatientIdentifierType;
+import org.openmrs.api.context.Context;
 import org.openmrs.module.reporting.dataset.DataSetColumn;
 import org.openmrs.module.reporting.dataset.DataSetRow;
 
@@ -28,11 +32,41 @@ public class ChronicCareMissedAppointmentBreakdownRenderer extends BreakdownRowR
 		addOutcomeCols(row, p, locationParameter,
 				lookupProgramWorkflow("Chronic care program", "Chronic care treatment status"));
 		addVhwCol(row, p);
+		addChronicCareCols(row, p);
 		addLastVisitCols(row, p, Arrays.asList(
 				lookupEncounterType("CHRONIC_CARE_FOLLOWUP")), "");
 		addVerificationCols(row, p);
 		
 		return row;
+	}
+
+	private void addChronicCareCols(DataSetRow row, Patient p) {
+		try {
+			DataSetColumn c;
+			List<Obs> obs;
+			Iterator<Obs> i;
+			c = new DataSetColumn("Status", "Status", String.class);
+			boolean deceased = (p.getDead() /*
+											 * || p.getCauseOfDeath() != null ||
+											 * p .getDeathDate() != null
+											 */);
+			row.addColumnValue(c, (deceased ? "died" : "&nbsp;"));
+
+			// cc diagnosis
+			String diag = "";
+			obs = Context.getObsService().getObservationsByPersonAndConcept(
+					p,
+					Context.getConceptService().getConceptByName(
+							"CHRONIC CARE DIAGNOSIS"));
+			i = obs.iterator();
+			while (i.hasNext()) {
+				diag += i.next().getValueAsString(Context.getLocale()) + ", ";
+			}
+			c = new DataSetColumn("Diagnosis", "Diagnosis", String.class);
+			row.addColumnValue(c, h(diag));
+		} catch (Exception e) {
+			log.error(e);
+		}
 	}
 
 	private void addVerificationCols(DataSetRow row, Patient p) {
