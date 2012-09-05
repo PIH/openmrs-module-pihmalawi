895f8272e2dd3a6b3faa502d2de0f59b652851ce
cneumann <cneumann@5bac5841-c719-aa4e-b3fe-cce5062f897a>
2012-09-05 18:49:14 +0000

    new tweaks

--- scripts/openmrs_watchdog.sh
+++ scripts/openmrs_watchdog.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Watchdog invoked"
+# Beware: mailx is a bitch to set up, in particular with gmail, check README.TXT for some hints
 
 MAIL=cneumann@pih.org
 PATH=$PATH:/bin:/usr/bin # make sure wget and mailx commands are in the crontab path
@@ -14,23 +14,26 @@ BASEDIR=$(dirname $0)
 #source $BASEDIR/openmrs.config
 source $BASEDIR/run_report.config
 
-wget --quiet --keep-session-cookies --save-cookies cookies.txt --post-data "uname=$USER&pw=$PW" $BASE_URL/loginServlet
+wget --quiet --post-data "uname=$USER&pw=$PW" $BASE_URL/loginServlet
 
 if [ $? -ne 0 ]; then
-  # stop and start tomcat, might/will require root privileges
-  # run script from root crontab or give sudo permissions to user (sudo visudo)
-  sudo /etc/init.d/tomcat5.5 stop
+  # do it again to make sure this system wasn't just too busy in this moment
   sleep 300
-  sudo /etc/init.d/tomcat5.5 start
-  echo "Sending out mail"
-  mailx -s "emr: OpenMRS did not respond. Restarted at `date +%Y%m%d-%H%M`" "$MAIL" <<EOF
-`tail -n 250  /var/log/tomcat5.5/catalina.out`
+  wget --quiet --post-data "uname=$USER&pw=$PW" $BASE_URL/loginServlet
+  if [ $? -ne 0 ]; then
+    # stop and start tomcat, might/will require root privileges
+    # run script from root crontab or give sudo permissions to user (sudo visudo)
+    sudo /etc/init.d/tomcat5.5 stop
+    sleep 300
+    sudo /etc/init.d/tomcat5.5 start
+    mailx -s "emr: OpenMRS did not respond. Restarted at `date +%Y%m%d-%H%M`" "$MAIL" <<EOF
+`tail -n 250 /var/log/tomcat5.5/catalina.out`
 EOF
+  fi
 else
-  #mailx -s "emr: OpenMRS running. All ok at `date +%Y%m%d-%H%M`" "$MAIL" <<EOF
-(intentionally empty)
-EOF
+#  mailx -s "emr: OpenMRS running. All ok at `date +%Y%m%d-%H%M`" "$MAIL" <<EOF
+#(intentionally empty)
+#EOF
 fi
 
-rm -f cookies.txt index.htm
-
+rm -f index.htm*
