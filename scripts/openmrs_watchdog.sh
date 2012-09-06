66e0eb5066ff6707919c384fe236f00b7433189e
cneumann <cneumann@5bac5841-c719-aa4e-b3fe-cce5062f897a>
2012-09-06 05:33:29 +0000

    area code added

--- scripts/openmrs_watchdog.sh
+++ scripts/openmrs_watchdog.sh
@@ -7,6 +7,7 @@ PATH=$PATH:/bin:/usr/bin # make sure wget and mailx commands are in the crontab 
 BASEDIR=$(dirname $0)
 
 # copy these 3 lines into a file openmrs.config and make sure it never gets checked in/published anywhere
+# AREA=UpperNeno
 # BASE_URL=http://localhost:8080/openmrs
 # USER=admin
 # PW=<change me>
@@ -22,12 +23,13 @@ if [ $? -ne 0 ]; then
   wget --quiet --post-data "uname=$USER&pw=$PW" $BASE_URL/loginServlet
   if [ $? -ne 0 ]; then
     # stop and start tomcat, might/will require root privileges
-    # run script from root crontab or give sudo permissions to user (sudo visudo)
+    # run script from root crontab or give sudo permissions to user
+    # e.g. 'emradmin ALL=NOPASSWD: /etc/init.d/tomcat5.5' via 'sudo visudo'
     sudo /etc/init.d/tomcat5.5 stop
     sleep 180
     sudo /etc/init.d/tomcat5.5 start
     sleep 60
-    mailx -s "emr: OpenMRS did not respond. Restarted at `date +%Y%m%d-%H%M`" "$MAIL" <<EOF
+    mailx -s "emr: `echo $AREA` OpenMRS did not respond. Restarted at `date +%Y%m%d-%H%M`" "$MAIL" <<EOF
 `tail -n 250 /var/log/tomcat5.5/catalina.out`
 EOF
   fi
