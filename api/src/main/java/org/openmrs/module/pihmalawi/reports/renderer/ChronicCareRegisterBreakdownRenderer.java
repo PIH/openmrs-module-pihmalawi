76da2e7a29babb971d1b3ba82f7207f1f8f23f0f
mseaton <mseaton@5bac5841-c719-aa4e-b3fe-cce5062f897a>
2013-03-12 21:15:34 +0000

    MLW-120 - Chronic care register report changes

--- api/src/main/java/org/openmrs/module/pihmalawi/reports/renderer/ChronicCareRegisterBreakdownRenderer.java
+++ api/src/main/java/org/openmrs/module/pihmalawi/reports/renderer/ChronicCareRegisterBreakdownRenderer.java
@@ -1,95 +1,112 @@
 package org.openmrs.module.pihmalawi.reports.renderer;
 
-import java.util.Arrays;
-import java.util.Date;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
-import org.openmrs.Location;
-import org.openmrs.Obs;
-import org.openmrs.Patient;
-import org.openmrs.PatientIdentifierType;
+import org.openmrs.*;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.reporting.dataset.DataSetColumn;
 import org.openmrs.module.reporting.dataset.DataSetRow;
 
 public class ChronicCareRegisterBreakdownRenderer extends BreakdownRowRenderer {
 
-	public DataSetRow renderRow(Patient p,
-			PatientIdentifierType patientIdentifierType,
-			Location locationParameter, Date startDateParameter,
-			Date endDateParameter) {
+	public DataSetRow renderRow(Patient p,PatientIdentifierType patientIdentifierType,Location locationParameter, Date startDateParameter,Date endDateParameter) {
 		DataSetRow row = new DataSetRow();
 
 		// exception handling looks really ugly, but its necessary...
 		try {
-			addCol(row, "National ID",
-					patientLink(p, patientIdentifierType, locationParameter));
-		} catch (Exception e) {
+			addCol(row, "National ID", patientLink(p, patientIdentifierType, locationParameter));
+		}
+		catch (Exception e) {
 			log.error(e);
 		}
 		try {
-			addCol(row, "All National IDs",
-					identifiers(p, patientIdentifierType));
-		} catch (Exception e) {
+			addCol(row, "All National IDs", identifiers(p, patientIdentifierType));
+		}
+		catch (Exception e) {
 			log.error(e);
 		}
 
-		addFirstEncounterCols(row, p,
-				lookupEncounterType("CHRONIC_CARE_INITIAL"),
-				"Chronic Care initial", endDateParameter);
-		addFirstChangeToStateCols(
-				row,
-				p,
-				lookupProgramWorkflowState("Chronic care program",
-						"Chronic care treatment status", "On treatment"),
-				endDateParameter,
-				"State");
+		addFirstEncounterCols(row, p,lookupEncounterType("CHRONIC_CARE_INITIAL"),"Chronic Care initial", endDateParameter);
+		addFirstChangeToStateCols(row,p,lookupProgramWorkflowState("Chronic care program","Chronic care treatment status", "On treatment"),endDateParameter,"State");
 		addDemographicCols(row, p, endDateParameter);
 		addDemographicTaDistrictCols(row, p, endDateParameter);
-		addOutcomeCols(
-				row,
-				p,
-				locationParameter,
-				endDateParameter,
-				lookupProgramWorkflow("Chronic care program",
-						"Chronic care treatment status"));
-		addChronicCareCols(row, p);
-		addMostRecentVitalsCols(row, p, endDateParameter);
+		addOutcomeCols(row,p,locationParameter,endDateParameter,lookupProgramWorkflow("Chronic care program","Chronic care treatment status"));
+
+		String[] diagnoses = {"Asthma","Diabetes","Epilepsy","Heart failure","Hypertension"};
+		for (int i=0; i<diagnoses.length; i++) {
+			String d = diagnoses[i];
+			String columnName = d + " Diagnosis from Initial Visit";
+			DataSetColumn column = new DataSetColumn(columnName, columnName, Boolean.class);
+			Object value = getFirstObsValue(p, "CHRONIC CARE DIAGNOSIS", d, "CHRONIC_CARE_INITIAL");
+			row.addColumnValue(column, value);
+		}
+
+		String[] diagnosisAges = {"Age of asthma diagnosis","Age of diabetes diagnosis","Age of epilepsy diagnosis","Age of heart failure diagnosis","Age of hypertension diagnosis"};
+		for (int i=0; i<diagnosisAges.length; i++) {
+			String question = diagnosisAges[i];
+			DataSetColumn column = new DataSetColumn(question + " from Initial Visit", question, Boolean.class);
+			Object value = getFirstObsValue(p, question, null, "CHRONIC_CARE_INITIAL");
+			row.addColumnValue(column, value);
+		}
+
 		addVhwCol(row, p);
-		addLastVisitCols(row, p, Arrays.asList(
-				lookupEncounterType("CHRONIC_CARE_FOLLOWUP")), "");
+		addLastVisitCols(row, p, Arrays.asList(lookupEncounterType("CHRONIC_CARE_FOLLOWUP")), "");
 		addAllEnrollmentsCol(row, p);
+		addMostRecentVitalsCols(row, p, endDateParameter);
+
+		{
+			DataSetColumn column = new DataSetColumn("HIV Status at Initial Visit", "HIV Status at Initial Visit", String.class);
+			Object value = getFirstObsValue(p, "HIV status", null, "CHRONIC_CARE_INITIAL");
+			row.addColumnValue(column, value);
+		}
+		{
+			DataSetColumn column = new DataSetColumn("On ART at Initial Visit", "On ART at Initial Visit", String.class);
+			Object value = getFirstObsValue(p, "On ART", null, "CHRONIC_CARE_INITIAL");
+			row.addColumnValue(column, value);
+		}
+		{
+			DataSetColumn column = new DataSetColumn("TB Status at Initial Visit", "TB Status at Initial Visit", String.class);
+			Object value = getFirstObsValue(p, "Previous or current tuberculosis treatment", null, "CHRONIC_CARE_INITIAL");
+			row.addColumnValue(column, value);
+		}
+		{
+			DataSetColumn column = new DataSetColumn("Clinic travel time in hours", "Clinic travel time in hours", Integer.class);
+			Object value = getFirstObsValue(p, "Clinic travel time in hours", null, "CHRONIC_CARE_INITIAL");
+			row.addColumnValue(column, value);
+		}
+		{
+			DataSetColumn column = new DataSetColumn("Clinic travel time in minutes", "Clinic travel time in minutes", Integer.class);
+			Object value = getFirstObsValue(p, "Clinic travel time in minutes", null, "CHRONIC_CARE_INITIAL");
+			row.addColumnValue(column, value);
+		}
+		{
+			DataSetColumn column = new DataSetColumn("High risk", "High risk", String.class);
+			Object value = getFirstObsValue(p, "High risk patient", null, null);
+			row.addColumnValue(column, value);
+		}
+
 		return row;
 	}
 
-	private void addChronicCareCols(DataSetRow row, Patient p) {
-		try {
-			DataSetColumn c;
-			List<Obs> obs;
-			Iterator<Obs> i;
-			c = new DataSetColumn("Status", "Status", String.class);
-			boolean deceased = (p.getDead() /*
-											 * || p.getCauseOfDeath() != null ||
-											 * p .getDeathDate() != null
-											 */);
-			row.addColumnValue(c, (deceased ? "died" : "&nbsp;"));
-
-			// cc diagnosis
-			String diag = "";
-			obs = Context.getObsService().getObservationsByPersonAndConcept(
-					p,
-					Context.getConceptService().getConceptByName(
-							"CHRONIC CARE DIAGNOSIS"));
-			i = obs.iterator();
-			while (i.hasNext()) {
-				diag += i.next().getValueAsString(Context.getLocale()) + ", ";
+	public Object getFirstObsValue(Person p, String conceptName, String answerConceptName, String encounterTypeName) {
+		List<Obs> obs = Context.getObsService().getObservationsByPersonAndConcept(p, lookupConcept(conceptName));
+		if (!obs.isEmpty()) {
+			Concept a = (answerConceptName != null ? lookupConcept(answerConceptName) : null);
+			EncounterType et = (encounterTypeName != null ? lookupEncounterType(encounterTypeName) : null);
+			for (Obs o : obs) {
+				if (a == null || a.equals(o.getValueCoded())) {
+					if (et == null || et.equals(o.getEncounter().getEncounterType())) {
+						if (o.getValueNumeric() != null) {
+							return o.getValueNumeric();
+						}
+						else {
+							return o.getValueAsString(Locale.ENGLISH);
+						}
+					}
+				}
 			}
-			c = new DataSetColumn("Diagnosis", "Diagnosis", String.class);
-			row.addColumnValue(c, h(diag));
-		} catch (Exception e) {
-			log.error(e);
 		}
+		return null;
 	}
 
 }
