70437bffe7940b761435beff977b62ada6e9ebe6
Cosmin Ioan <cioan@pih.org>
2017-09-05 12:25:04 -0400

    MLW-539, record Health Facility when registering CHW

--- api/src/main/java/org/openmrs/module/pihmalawi/PihMalawiConstants.java
+++ api/src/main/java/org/openmrs/module/pihmalawi/PihMalawiConstants.java
@@ -6,5 +6,7 @@ public class PihMalawiConstants {
     public static final String MODULE_ID = "pihmalawi";
     public static final String OPENMRS_WAREHOUSE_CONNECTION_PROPERTIES_FILE_NAME = "warehouse-connection.properties";
     public static final String PRIV_CHW_MANAGEMENT_APP = "CHW Management App";
+    public static final String HEALTH_FACILITY_GP_NAME = "providermanagement.locationTag";
+    public static final String HEALTH_FACILITY_GP_VALUE = "Health Facility";
 
 }
--- api/src/main/java/org/openmrs/module/pihmalawi/metadata/deploy/bundle/ProviderAttributeTypeBundle.java
+++ api/src/main/java/org/openmrs/module/pihmalawi/metadata/deploy/bundle/ProviderAttributeTypeBundle.java
@@ -2,13 +2,17 @@ package org.openmrs.module.pihmalawi.metadata.deploy.bundle;
 
 
 import org.openmrs.Concept;
+import org.openmrs.Location;
 import org.openmrs.ProviderAttributeType;
 import org.openmrs.api.ConceptService;
 import org.openmrs.api.ProviderService;
+import org.openmrs.api.context.Context;
 import org.openmrs.customdatatype.datatype.DateDatatype;
 import org.openmrs.customdatatype.datatype.FreeTextDatatype;
 import org.openmrs.module.coreapps.customdatatype.CodedConceptDatatype;
+import org.openmrs.module.coreapps.customdatatype.LocationDatatype;
 import org.openmrs.module.metadatadeploy.bundle.AbstractMetadataBundle;
+import org.openmrs.module.pihmalawi.PihMalawiConstants;
 import org.openmrs.web.attribute.handler.DateFieldGenDatatypeHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -32,6 +36,7 @@ public class ProviderAttributeTypeBundle extends AbstractMetadataBundle {
         public static final String NUMBER_OF_HOUSEHOLDS  = "0c267ae8-f793-4cf8-9b27-93accaa45d86";
         public static final String DATE_HIRED  = "c8ef8a16-a8cd-4748-b0ea-e8a1ec503fbb";
         public static final String HH_MODEL_TEST  = "C0E1F105-DD36-4577-B00E-87A08D446A3A";
+        public static final String HEALTH_FACILITY  = "94047146-7918-4927-9401-F4284A10C7FD";
 
         public static final String HH_MODEL_TEST_CONCEPT  = "0E483511-6278-4D1A-881A-6385C223FAC7";
 
@@ -91,6 +96,16 @@ public class ProviderAttributeTypeBundle extends AbstractMetadataBundle {
                 0,
                 1,
                 ProviderAttributeTypes.HH_MODEL_TEST));
+
+
+        install(providerAttributeType(
+                PihMalawiConstants.HEALTH_FACILITY_GP_VALUE,
+                PihMalawiConstants.HEALTH_FACILITY_GP_VALUE,
+                LocationDatatype.class,
+                PihMalawiConstants.HEALTH_FACILITY_GP_VALUE,
+                0,
+                1,
+                ProviderAttributeTypes.HEALTH_FACILITY));
 
     }
 }
