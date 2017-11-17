191b64560d41d4e83d41bfab7f267964bb65c5d1
Cosmin Ioan <cioan@pih.org>
2017-11-17 14:12:46 -0500

    MLW-511, add PCC to the dashboard

--- api/src/main/java/org/openmrs/module/pihmalawi/PihMalawiConstants.java
+++ api/src/main/java/org/openmrs/module/pihmalawi/PihMalawiConstants.java
@@ -8,5 +8,9 @@ public class PihMalawiConstants {
     public static final String PRIV_CHW_MANAGEMENT_APP = "CHW Management App";
     public static final String HEALTH_FACILITY_GP_NAME = "providermanagement.locationTag";
     public static final String HEALTH_FACILITY_GP_VALUE = "Health Facility";
+    public static final String DASHBOARD_IDENTIFIERS_GP_NAME = "dashboard.identifiers";
+    public static final String DASHBOARD_IDENTIFIERS_GP_VALUE = "{\"9\":[\"ARV Number\",\"HCC Number\",\"KS Number\",\"Chronic Care Number\",\"Palliative Care Number\"]}";
+    public static final String PATIENT_IDENTIFIER_IMPORTANT_TYPES_GP_NAME = "patient_identifier.importantTypes";
+    public static final String PATIENT_IDENTIFIER_IMPORTANT_TYPES_GP_VALUE = "ARV Number,HCC Number,Chronic Care Number,Palliative Care Number";
 
 }
