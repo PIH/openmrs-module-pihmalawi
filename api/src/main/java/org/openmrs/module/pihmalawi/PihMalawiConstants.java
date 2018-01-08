d9797fcb78f9518c6a948499145ce483a0dfbd85
Cosmin Ioan <cioan@pih.org>
2018-01-08 13:26:51 -0500

    MLW-664, disable entering new Viral Load Tests encounters

--- api/src/main/java/org/openmrs/module/pihmalawi/PihMalawiConstants.java
+++ api/src/main/java/org/openmrs/module/pihmalawi/PihMalawiConstants.java
@@ -6,6 +6,7 @@ public class PihMalawiConstants {
     public static final String MODULE_ID = "pihmalawi";
     public static final String OPENMRS_WAREHOUSE_CONNECTION_PROPERTIES_FILE_NAME = "warehouse-connection.properties";
     public static final String PRIV_CHW_MANAGEMENT_APP = "CHW Management App";
+    public static final String SAMPLE_TAKEN_FOR_VIRAL_LOAD = "f792f2f9-9c24-4d6e-98fd-caffa8f2383f";
     public static final String HEALTH_FACILITY_GP_NAME = "providermanagement.locationTag";
     public static final String HEALTH_FACILITY_GP_VALUE = "Health Facility";
     public static final String DASHBOARD_IDENTIFIERS_GP_NAME = "dashboard.identifiers";
