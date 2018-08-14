06f45250be9b563c7c9e726aafabf1dc843db6d9
lnball <eball@pih.org>
2018-08-14 17:41:32 -0400

    MLW-737 Added concepts for the workflow

--- api/src/main/java/org/openmrs/module/pihmalawi/metadata/deploy/bundle/PihMalawiMetadataBundle.java
+++ api/src/main/java/org/openmrs/module/pihmalawi/metadata/deploy/bundle/PihMalawiMetadataBundle.java
@@ -6,12 +6,13 @@ import org.openmrs.module.metadatadeploy.bundle.Requires;
 import org.openmrs.module.pihmalawi.metadata.PalliativeCareMetadata;
 import org.openmrs.module.pihmalawi.metadata.PihMalawiPatientIdentifierTypes;
 import org.openmrs.module.pihmalawi.metadata.deploy.bundle.concept.ChwManagementConcepts;
+import org.openmrs.module.pihmalawi.metadata.deploy.bundle.concept.IC3ScreeningConcepts;
 import org.openmrs.module.pihmalawi.metadata.deploy.bundle.concept.MasterCardConcepts;
 import org.openmrs.module.pihmalawi.metadata.deploy.bundle.concept.PalliativeCareConcepts;
 import org.springframework.stereotype.Component;
 
 @Component
-@Requires( {PalliativeCareConcepts.class, MasterCardConcepts.class, ChwManagementConcepts.class} )
+@Requires( {PalliativeCareConcepts.class, MasterCardConcepts.class, ChwManagementConcepts.class, IC3ScreeningConcepts.class} )
 public class PihMalawiMetadataBundle extends AbstractMetadataBundle{
 
     @Override
