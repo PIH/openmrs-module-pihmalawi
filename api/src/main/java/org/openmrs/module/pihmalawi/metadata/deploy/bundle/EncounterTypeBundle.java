467a92919631623d5bb9994a165ef2faa20f3973
Cosmin Ioan <cioan@pih.org>
2018-09-06 08:34:11 +0200

    MLW-754, add DNA-PCR test encounter

--- api/src/main/java/org/openmrs/module/pihmalawi/metadata/deploy/bundle/EncounterTypeBundle.java
+++ api/src/main/java/org/openmrs/module/pihmalawi/metadata/deploy/bundle/EncounterTypeBundle.java
@@ -69,6 +69,7 @@ public class EncounterTypeBundle extends AbstractMetadataBundle {
         install(EncounterTypes.HTC_SCREENING);
         install(EncounterTypes.VIRAL_LOAD_SCREENING);
         install(EncounterTypes.EID_LOAD_SCREENING);
+        install(EncounterTypes.DNA_PCR_SCREENING);
     }
 
     //***** BUNDLE INSTALLATION METHODS FOR DESCRIPTORS
