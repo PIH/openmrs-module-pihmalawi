25cc78aa86a4817e443a0ab15791bbc09641ccd1
mgoodrich <mark.goodrich@live.com>
2019-01-10 17:23:38 -0500

    update IC3 Screening Data report to include all identifiers with location and types

---
+++ api/src/main/java/org/openmrs/module/pihmalawi/reporting/definition/data/converter/PatientIdentifierToJsonConverter.java
@@ -0,0 +1,31 @@
+package org.openmrs.module.pihmalawi.reporting.definition.data.converter;
+
+import org.openmrs.PatientIdentifier;
+import org.openmrs.module.pihmalawi.common.JsonObject;
+import org.openmrs.module.reporting.data.converter.DataConverter;
+
+public class PatientIdentifierToJsonConverter implements DataConverter {
+
+    public PatientIdentifierToJsonConverter() {};
+
+    @Override
+    public Object convert(Object o) {
+        PatientIdentifier identifier = (PatientIdentifier) o;
+        JsonObject result = new JsonObject();
+        result.put("location", identifier.getLocation());
+        result.put("identifierType", identifier.getIdentifierType());
+        result.put("identifier", new PatientIdentifierConverter().convert(identifier));
+        result.put("raw_identifier", identifier.getIdentifier());
+        return result;
+    }
+
+    @Override
+    public Class<?> getInputDataType() {
+        return PatientIdentifier.class;
+    }
+
+    @Override
+    public Class<?> getDataType() {
+        return JsonObject.class;
+    }
+}
