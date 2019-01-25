3fc250c123f1c7d94d7cd5bb58919f8e312f37fc
mgoodrich <mark.goodrich@live.com>
2019-01-25 13:54:28 -0500

    rename last_muac to current_muac; as isFalse and isTrue functions

--- api/src/main/java/org/openmrs/module/pihmalawi/data/IC3ScreeningData.java
+++ api/src/main/java/org/openmrs/module/pihmalawi/data/IC3ScreeningData.java
@@ -102,7 +102,7 @@ public class IC3ScreeningData extends LivePatientDataSet {
         addColumn(dsd, "last_creatinine_result_date", df.getMostRecentObsByEndDate(ccMetadata.getCreatinineConcept(), null, df.getObsDatetimeConverter()));
         addColumn(dsd, "last_creatinine_result", df.getMostRecentObsByEndDate(ccMetadata.getCreatinineConcept(), null, df.getObsValueNumericConverter()));
         addColumn(dsd, "last_bmi", basePatientData.getLatestBmiNumericValueByEndDate());
-        addColumn(dsd, "last_muac", df.getMostRecentObsOnDate(ccMetadata.getMUACConcept(), null, df.getObsValueNumericConverter()));
+        addColumn(dsd, "current_muac", df.getMostRecentObsOnDate(ccMetadata.getMUACConcept(), null, df.getObsValueNumericConverter()));
         addColumn(dsd, "family_history_diabetes", ccPatientData.getFamilyHistoryOfDiabetesByEndDate());
         addColumn(dsd, "last_breastfeeding_status", hivPatientData.getLatestBreastfeedingStatusValueByEndDate());
         addColumn(dsd, "is_pregnant", df.getMostRecentObsOnDate(ccMetadata.getIsPatientPregnantConcept(), null, df.getObsValueCodedConverter()));
