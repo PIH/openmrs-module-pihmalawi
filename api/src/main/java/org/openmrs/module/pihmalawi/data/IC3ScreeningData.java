13bf7574288a1c785be3a6f24703b4970465dcea
mgoodrich <mark.goodrich@live.com>
2019-02-05 17:14:25 -0500

    add deceased to IC3ScreeningData

--- api/src/main/java/org/openmrs/module/pihmalawi/data/IC3ScreeningData.java
+++ api/src/main/java/org/openmrs/module/pihmalawi/data/IC3ScreeningData.java
@@ -72,6 +72,7 @@ public class IC3ScreeningData extends LivePatientDataSet {
         addColumn(dsd, "age_years", basePatientData.getAgeAtEndInYears());
         addColumn(dsd, "age_months", basePatientData.getAgeAtEndInMonths());
         addColumn(dsd, "age_days", basePatientData.getAgeAtEndInDays());
+        addColumn(dsd, "deceased", builtInPatientData.getVitalStatusDead());
         addColumn(dsd, "identifiers", basePatientData.getAllIdentifiers());
         addColumn(dsd, "hcc_number", hivPatientData.getHccNumberAtLocation()); // TODO: See if this is what we want
         addColumn(dsd, "art_number", hivPatientData.getArvNumberAtLocation()); // TODO: See if this is what we want
