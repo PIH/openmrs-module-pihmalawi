064a00902416f11748dba472aacda27a87d4b943
mgoodrich <mark.goodrich@live.com>
2019-04-02 16:40:46 -0400

    IS-311: Clarify the format of the temp identifier we use for Malawi

--- api/src/main/java/org/openmrs/module/pihmalawi/validator/MalawiPatientValidator.java
+++ api/src/main/java/org/openmrs/module/pihmalawi/validator/MalawiPatientValidator.java
@@ -10,10 +10,15 @@ import org.openmrs.module.pihmalawi.metadata.PihMalawiPatientIdentifierTypes;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.UUID;
 
+/**
+ * This is support the registration of new patients at the IC3 clinic who have yet to be enrolled in a program.
+ * If the user selects "IC3 Identifier Type" when registering a patient, but does *not* enter an identifier,
+ * the system will assign one using the following pattern:
+ *
+ * IC3-{first two digits of location id, with a padded zero if needed}{4 digits from a random uuid}
+ */
 @Handler(supports = { Patient.class }, order = 10)
 public class MalawiPatientValidator implements Validator {
 
@@ -34,7 +39,9 @@ public class MalawiPatientValidator implements Validator {
         for (PatientIdentifier patientIdentifier : patient.getIdentifiers()) {
             if (StringUtils.isBlank(patientIdentifier.getIdentifier()) && patientIdentifier.getIdentifierType().equals(ic3IdentifierType)) {
                 String uuid = UUID.randomUUID().toString();
-                patientIdentifier.setIdentifier("IC3-" + patientIdentifier.getLocation().getId().toString() + "-" + new SimpleDateFormat("yyyy-MM-dd-").format(new Date()) + uuid.substring(1,2) + uuid.substring(11,12));
+                patientIdentifier.setIdentifier("IC3-"
+                        + StringUtils.leftPad(patientIdentifier.getLocation().getId().toString(), 2, "0").substring(0,2).toUpperCase()
+                        + uuid.substring(1,3).toUpperCase() + uuid.substring(24,26).toUpperCase());
             }
         }
     }
