588df3abf33a5070f110a9c1b59f4febd4cc1ad9
Stephen Po-Chedley <pochedley@gmail.com>
2019-04-23 12:40:22 -0400

    MLW-831 added consecutive high viral load report
    (cherry picked from commit 146e8cbe64d06235f5c0172cf00bcd79a094ced1)

---
+++ api/src/main/java/org/openmrs/module/pihmalawi/reporting/reports/HivConsecutiveHighViralLoadReport.java
@@ -0,0 +1,89 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.pihmalawi.reporting.reports;
+
+import org.openmrs.module.pihmalawi.PihMalawiConstants;
+import org.openmrs.module.pihmalawi.reporting.definition.dataset.definition.SqlFileDataSetDefinition;
+import org.openmrs.module.reporting.ReportingConstants;
+import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
+import org.openmrs.module.reporting.evaluation.parameter.Mapped;
+import org.openmrs.module.reporting.evaluation.parameter.Parameter;
+import org.openmrs.module.reporting.report.ReportDesign;
+import org.openmrs.module.reporting.report.definition.ReportDefinition;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class HivConsecutiveHighViralLoadReport extends ApzuReportManager {
+
+    public static final String SQL_DATA_SET_RESOURCE = "org/openmrs/module/pihmalawi/reporting/datasets/sql/consecutive-high-viral-load.sql";
+    public static final String EXCEL_REPORT_DESIGN_UUID = "1c1ff582-6510-11e9-9c3a-dbb5f9c0d8f7";
+    public static final Parameter VIRAL_LOAD = new Parameter("viralLoad", "Viral Load Cutoff", Integer.class);
+
+	@Override
+	public String getUuid() {
+		return "f2659c14-651f-11e9-9c3a-dbb5f9c0d8f7";
+	}
+
+	@Override
+	public String getName() {
+		return "HIV - Consecutive High Viral Load Report";
+	}
+
+    @Override
+    public String getDescription() {
+        return "Patients with two consecutive high viral load results";
+    }
+
+    @Override
+    public List<Parameter> getParameters() {
+        List<Parameter> l = new ArrayList<Parameter>();
+        l.add(ReportingConstants.END_DATE_PARAMETER);
+        l.add(VIRAL_LOAD);
+        return l;
+    }
+
+    /**
+     * @see ApzuReportManager#constructReportDefinition()
+     */
+    @Override
+    public ReportDefinition constructReportDefinition() {
+        ReportDefinition rd = new ReportDefinition();
+        rd.setUuid(getUuid());
+        rd.setName(getName());
+        rd.setDescription(getDescription());
+        rd.setParameters(getParameters());
+        rd.addDataSetDefinition("consecutiveViralLoad", Mapped.mapStraightThrough(constructDataSetDefinition()));
+        return rd;
+    }
+
+    public DataSetDefinition constructDataSetDefinition() {
+        SqlFileDataSetDefinition dsd = new SqlFileDataSetDefinition();
+        dsd.setParameters(getParameters());
+        dsd.setConnectionPropertyFile(PihMalawiConstants.OPENMRS_WAREHOUSE_CONNECTION_PROPERTIES_FILE_NAME);
+        dsd.setSqlResource(SQL_DATA_SET_RESOURCE);
+        return dsd;
+    }
+
+    @Override
+    public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
+        List<ReportDesign> l = new ArrayList<ReportDesign>();
+        ReportDesign design = createExcelTemplateDesign(EXCEL_REPORT_DESIGN_UUID, reportDefinition, "ConsecutiveViralLoadReport.xls");
+        design.addPropertyValue("repeatingSections", "sheet:1,row:5,dataset:consecutiveViralLoad");
+        l.add(design);
+        return l;
+    }
+}
