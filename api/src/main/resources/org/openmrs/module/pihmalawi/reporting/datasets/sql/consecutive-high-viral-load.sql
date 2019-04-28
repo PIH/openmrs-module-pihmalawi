159909801b47e93ac6f1dce70a9c66d8548849de
Stephen Po-Chedley <pochedley@gmail.com>
2019-04-28 15:57:42 -0700

    MLW-831: use sample date, add regimen, filter for active patients

--- api/src/main/resources/org/openmrs/module/pihmalawi/reporting/datasets/sql/consecutive-high-viral-load.sql
+++ api/src/main/resources/org/openmrs/module/pihmalawi/reporting/datasets/sql/consecutive-high-viral-load.sql
@@ -17,7 +17,8 @@ SELECT mlt.patient_id - 10000000 as pid,
 	   mp.first_name as first,
 	   mp.last_name as last,
 	   mp.gender,
-	   floor(datediff(@endDate,mp.birthdate)/365.25) as age,	   
+	   floor(datediff(@endDate,mp.birthdate)/365.25) as age,
+	   regimen, 	   
 	   mp.traditional_authority as ta,
 	   mp.village,
 	   mp.vhw,	   
@@ -28,10 +29,16 @@ SELECT mlt.patient_id - 10000000 as pid,
 FROM mw_lab_tests mlt
 JOIN (select patient_id as pid, result_numeric as previous, date_collected as previous_date 
 	  from mw_lab_tests 
-	  where lab_test_id = latest_test_result_by_date_entered(patient_id, "Viral Load", @startDate, @endDate, 1)) mlt2 
-ON mlt2.pid = mlt.patient_id
+	  where lab_test_id = latest_test_result_by_sample_date(patient_id, "Viral Load", @startDate, @endDate, 1)) mlt2 
+	ON mlt2.pid = mlt.patient_id
+JOIN mw_art_register mar on mar.patient_id = mlt.patient_id
 JOIN mw_patient mp on mp.patient_id = mlt.patient_id
-WHERE lab_test_id = latest_test_result_by_date_entered(mlt.patient_id, "Viral Load", @startDate, @endDate, 0)
+JOIN (select * from 
+		(select patient_id, value_coded as regimen from omrs_obs where concept like 'Malawi Antiretroviral drugs%' and obs_date <= @endDate order by obs_date desc) oi 
+	  group by patient_id) mo
+	  ON mo.patient_id = mlt.patient_id
+WHERE lab_test_id = latest_test_result_by_sample_date(mlt.patient_id, "Viral Load", @startDate, @endDate, 0)
+AND (outcome = "On antiretrovirals" OR outcome is NULL)
 HAVING result_numeric > @viralLoad
 AND previous > @viralLoad
 ORDER BY mlt.date_collected desc;
