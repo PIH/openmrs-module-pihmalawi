c47c3723e22af7564ed920d268bee5766ef7cbb5
Stephen Po-Chedley <pochedley@gmail.com>
2019-05-05 17:09:11 -0700

    MLW-831: add regimen start date

--- api/src/main/resources/org/openmrs/module/pihmalawi/reporting/datasets/sql/consecutive-high-viral-load.sql
+++ api/src/main/resources/org/openmrs/module/pihmalawi/reporting/datasets/sql/consecutive-high-viral-load.sql
@@ -9,16 +9,28 @@
 
 *************************************************************************/
 
-
+# Need to include start date since this is not a user specified parameter
 set @startDate = "1900-01-01";
 
+# create a temporary lookup table with regiment information
+drop table if exists regimenStartTable;
+create temporary table regimenStartTable as
+select patient_id, value_coded as regimen, obs_date as start_date from (select * 
+	from omrs_obs 
+	where concept like 'Malawi Antiretroviral drugs%'
+	and obs_date <= @endDate
+	order by obs_date asc) i
+group by patient_id, value_coded;
+
+# main query 
 SELECT mlt.patient_id - 10000000 as pid, 
 	   mp.identifier as id,
 	   mp.first_name as first,
 	   mp.last_name as last,
 	   mp.gender,
 	   floor(datediff(@endDate,mp.birthdate)/365.25) as age,
-	   regimen, 	   
+	   rst.regimen,
+	   rst.start_date as regimenStartDate,
 	   mp.traditional_authority as ta,
 	   mp.village,
 	   mp.vhw,	   
@@ -37,8 +49,12 @@ JOIN (select * from
 		(select patient_id, value_coded as regimen from omrs_obs where concept like 'Malawi Antiretroviral drugs%' and obs_date <= @endDate order by obs_date desc) oi 
 	  group by patient_id) mo
 	  ON mo.patient_id = mlt.patient_id
+JOIN regimenStartTable rst on rst.patient_id = mlt.patient_id AND mo.regimen = rst.regimen	  
 WHERE lab_test_id = latest_test_result_by_sample_date(mlt.patient_id, "Viral Load", @startDate, @endDate, 0)
 AND (outcome = "On antiretrovirals" OR outcome is NULL)
 HAVING result_numeric > @viralLoad
 AND previous > @viralLoad
 ORDER BY mlt.date_collected desc;
+
+# clean up
+drop table if exists regimenStartTable;
