840bb2ad7b7d42009f17bff1a66f6a9f69618e61
Stephen Po-Chedley <pochedley@gmail.com>
2019-06-02 18:04:28 -0700

    Updates to Consecutive High Viral Load [MLW-1079]

--- api/src/main/resources/org/openmrs/module/pihmalawi/reporting/datasets/sql/consecutive-high-viral-load.sql
+++ api/src/main/resources/org/openmrs/module/pihmalawi/reporting/datasets/sql/consecutive-high-viral-load.sql
@@ -12,15 +12,25 @@
 # Need to include start date since this is not a user specified parameter
 set @startDate = "1900-01-01";
 
-# create a temporary lookup table with regiment information
+# create a temporary lookup table with regimen information
 drop table if exists regimenStartTable;
 create temporary table regimenStartTable as
 select patient_id, value_coded as regimen, obs_date as start_date from (select * 
 	from omrs_obs 
 	where concept like 'Malawi Antiretroviral drugs%'
 	and obs_date <= @endDate
-	order by obs_date asc) i
+	order by obs_date desc) i
 group by patient_id, value_coded;
+
+CREATE INDEX idx ON regimenStartTable (patient_id);
+
+# duplicate this table
+# need to do this because otherwise the main query will fail trying to open regimenStartTable twice in the main query
+drop table if exists regimenStartTable2;
+create temporary table regimenStartTable2 as
+select * from regimenStartTable; 
+
+CREATE INDEX idx ON regimenStartTable2 (patient_id);
 
 # main query 
 SELECT mlt.patient_id - 10000000 as pid, 
@@ -29,8 +39,13 @@ SELECT mlt.patient_id - 10000000 as pid,
 	   mp.last_name as last,
 	   mp.gender,
 	   floor(datediff(@endDate,mp.birthdate)/365.25) as age,
-	   rst.regimen,
-	   rst.start_date as regimenStartDate,
+	   visit_date as lastVisitDate,
+	   mav.location as lastVisitLocation,
+	   next_appointment_date as nextApptDate,
+	   get_regimen(mlt.patient_id, 0) as currentRegimen,	
+	   get_regimen_start(mlt.patient_id, 0) as currentRegimenStartDate,
+	   get_regimen(mlt.patient_id, 1) as previousRegimen,
+	   get_regimen_start(mlt.patient_id, 1) as previousRegimenStartDate,	
 	   mp.traditional_authority as ta,
 	   mp.village,
 	   mp.vhw,	   
@@ -43,13 +58,16 @@ JOIN (select patient_id as pid, result_numeric as previous, date_collected as pr
 	  from mw_lab_tests 
 	  where lab_test_id = latest_test_result_by_sample_date(patient_id, "Viral Load", @startDate, @endDate, 1)) mlt2 
 	ON mlt2.pid = mlt.patient_id
+JOIN (select patient_id, visit_date, location, next_appointment_date 
+	  from (select * from mw_art_visits where visit_date < @endDate order by visit_date desc) mavi
+	  group by patient_id) mav
+	ON mav.patient_id = mlt.patient_id	  
 JOIN mw_art_register mar on mar.patient_id = mlt.patient_id
 JOIN mw_patient mp on mp.patient_id = mlt.patient_id
 JOIN (select * from 
 		(select patient_id, value_coded as regimen from omrs_obs where concept like 'Malawi Antiretroviral drugs%' and obs_date <= @endDate order by obs_date desc) oi 
 	  group by patient_id) mo
 	  ON mo.patient_id = mlt.patient_id
-JOIN regimenStartTable rst on rst.patient_id = mlt.patient_id AND mo.regimen = rst.regimen	  
 WHERE lab_test_id = latest_test_result_by_sample_date(mlt.patient_id, "Viral Load", @startDate, @endDate, 0)
 AND (outcome = "On antiretrovirals" OR outcome is NULL)
 HAVING result_numeric > @viralLoad
@@ -57,4 +75,5 @@ AND previous > @viralLoad
 ORDER BY mlt.date_collected desc;
 
 # clean up
-drop table if exists regimenStartTable;
+DROP TABLE IF EXISTS regimenStartTable;
+DROP TABLE IF EXISTS regimenStartTable2;
