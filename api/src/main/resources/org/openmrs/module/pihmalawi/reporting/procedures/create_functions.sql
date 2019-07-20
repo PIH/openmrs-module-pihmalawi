efdadcd1b603214f20b5db05b46319b70d5aae05
Michael Seaton <mseaton@pih.org>
2019-07-19 23:34:02 -0400

    Start creating reusable functions and procedures for creating temporary tables for reporting

---
+++ api/src/main/resources/org/openmrs/module/pihmalawi/reporting/procedures/create_functions.sql
@@ -0,0 +1,35 @@
+/*
+    Return the concept id for the given uuid
+*/
+DROP FUNCTION IF EXISTS concept_by_uuid;
+CREATE FUNCTION concept_by_uuid(_uuid CHAR(38))
+    RETURNS INT
+    DETERMINISTIC
+BEGIN
+    DECLARE ret INT;
+
+    SELECT concept_id into ret
+        FROM concept
+        WHERE uuid = _uuid;
+
+    return ret;
+END
+;
+
+/*
+    Return the patient_identifier_type id for the given uuid
+*/
+DROP FUNCTION IF EXISTS identifier_type_by_uuid;
+CREATE FUNCTION identifier_type_by_uuid(_uuid CHAR(38))
+    RETURNS INT
+    DETERMINISTIC
+BEGIN
+    DECLARE ret INT;
+
+    SELECT patient_identifier_type_id into ret
+        FROM patient_identifier_type
+        WHERE uuid = _uuid;
+
+    return ret;
+END
+;
