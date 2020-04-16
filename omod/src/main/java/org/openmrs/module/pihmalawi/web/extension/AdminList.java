dfb08fdeadbfc1290475b8f5250b5f023938efcd
Cosmin Ioan <cioan@pih.org>
2020-04-15 20:02:24 -0400

    MLW-1127, add Import VL Results link to Admin screen

--- omod/src/main/java/org/openmrs/module/pihmalawi/web/extension/AdminList.java
+++ omod/src/main/java/org/openmrs/module/pihmalawi/web/extension/AdminList.java
@@ -55,6 +55,7 @@ public class AdminList extends AdministrationSectionExt {
 			map.put("/coreapps/providermanagement/providerList.page", "List CHW");
 			map.put("/coreapps/providermanagement/editProvider.page", "Create CHW");
 		}
+		map.put("/pihmalawi/vlresults/importVLR.page", "Import VL Results");
 		map.put("/pihmalawi/setupReports.page", "Create or Delete Reports");
 		
 		return map;
