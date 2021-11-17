e56a9518fc2049601b731ec0a9b57a206c98c6dc
kmatiya <khormatiya@gmail.com>
2021-11-17 11:53:38 +0200

    Rename PDC_CLEFT_CLIP_PALLET to PDC_CLEFT_CLIP_PALLET_INITIAL

--- api/src/main/java/org/openmrs/module/pihmalawi/metadata/deploy/bundle/EncounterTypeBundle.java
+++ api/src/main/java/org/openmrs/module/pihmalawi/metadata/deploy/bundle/EncounterTypeBundle.java
@@ -94,7 +94,7 @@ public class EncounterTypeBundle extends AbstractMetadataBundle {
         install(EncounterTypes.PDC_FOLLOWUP);
         install(EncounterTypes.PDC_TRISOMY21);
         install(EncounterTypes.PDC_TRISOMY21_FOLLOWUP);
-        install(EncounterTypes.PDC_CLEFT_CLIP_PALLET);
+        install(EncounterTypes.PDC_CLEFT_CLIP_PALLET_INITIAL);
         install(EncounterTypes.PDC_DEVELOPMENTAL_DELAY_INITIAL);
         install(EncounterTypes.PDC_DEVELOPMENTAL_DELAY_FOLLOWUP);
 
