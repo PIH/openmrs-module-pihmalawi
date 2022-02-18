09d329abbdc2f37b41ab781acbb8557211f798a5
Cosmin Ioan <cioan@pih.org>
2022-02-18 18:06:09 -0500

    MLW-1311, add diabetes indicators

--- api/src/main/resources/org/openmrs/module/pihmalawi/reporting/datasets/sql/ncd-quarterly.sql
+++ api/src/main/resources/org/openmrs/module/pihmalawi/reporting/datasets/sql/ncd-quarterly.sql
@@ -30,7 +30,7 @@ CREATE TABLE rpt_ic3_indicators (
 DELETE from rpt_ic3_indicators WHERE indicator = 'NCD-H1N';
 INSERT INTO rpt_ic3_indicators
 (indicator, description, indicator_type, indicator_value)
-  SELECT 'NCD-H1N', 'Patients enrolled and active in care', 'At date', count(*)
+  SELECT 'NCD-H1N', 'Hypertension patients enrolled and active in care', 'At date', count(*)
   FROM 	rpt_ic3_data_table
   WHERE 	currentNcdState = "On treatment"
          AND htnDx is NOT NULL
@@ -44,7 +44,7 @@ INSERT INTO rpt_ic3_indicators
 DELETE from rpt_ic3_indicators WHERE indicator = 'NCD-H2N';
 INSERT INTO rpt_ic3_indicators
 (indicator, description, indicator_type, indicator_value)
-  SELECT 'NCD-H2N', 'Patients newly registered during reporting period',
+  SELECT 'NCD-H2N', 'Hypertension patients newly registered during reporting period',
     'At date', count(*)
   FROM 	rpt_ic3_data_table
   WHERE 	currentNcdState = "On treatment"
@@ -241,7 +241,8 @@ INSERT INTO rpt_ic3_indicators
 
 /*
 	NCD-A6N - Asthma patients with disease controlled (severity at intermittent or "mild persistent" at last visit)
-	Active asthma diagnosed patients who had disease severity observation recorded at last visit  (concept: 8410) and the answer was severity at "intermittent" or "mild persistent"
+	Active asthma diagnosed patients who had disease severity observation recorded at last visit  (concept: 8410)
+	and the answer was severity at "intermittent" or "mild persistent"
 
 */
 DELETE from rpt_ic3_indicators WHERE indicator = 'NCD-A6N';
@@ -265,7 +266,7 @@ INSERT INTO rpt_ic3_indicators
 DELETE from rpt_ic3_indicators WHERE indicator = 'NCD-A7N';
 INSERT INTO rpt_ic3_indicators
 (indicator, description, indicator_type, indicator_value)
-  SELECT 'NCD-A7N', 'Patients hospitalized for the condition since last visit ',
+  SELECT 'NCD-A7N', 'Asthma patients hospitalized for the condition since last visit ',
     'At date', count(*)
   FROM rpt_ic3_data_table r, omrs_encounter e
   WHERE r.patient_id = e.patient_id
@@ -275,6 +276,242 @@ INSERT INTO rpt_ic3_indicators
         AND r.lastAsthmaVisitDate is not null
         AND @startDate <= r.lastAsthmaVisitDate and r.lastAsthmaVisitDate <= @endDate
         AND r.ncdCurrentLocation=@location
+;
+
+/*
+	NCD-D1N - Diabetes patients enrolled and active in care
+	Number of patients with a chronic care diagnosis of "Type 1 diabetes" or "Type 2 diabetes"
+	who have an "On treatment" status for the chronic care program at the facility on report end date
+*/
+DELETE from rpt_ic3_indicators WHERE indicator = 'NCD-D1N';
+INSERT INTO rpt_ic3_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'NCD-D1N', 'Diabetes patients enrolled and active in care', 'At date', count(*)
+  FROM 	rpt_ic3_data_table
+  WHERE 	currentNcdState = "On treatment"
+         AND dmDx is NOT NULL
+         AND ncdCurrentLocation=@location
+;
+
+/*
+	NCD-D2N - Diabetes patients newly registered during reporting period
+	Patients with a new chronic care diagnosis of "Type 1 Diabetes" in the last 3 months up to end date for report
+	+ (PLUS) Patients with a new chronic care diagnosis of "Type 2 Diabetes" in the last 3 months up to end date for report
+*/
+DELETE from rpt_ic3_indicators WHERE indicator = 'NCD-D2N';
+INSERT INTO rpt_ic3_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'NCD-D2N', 'Diabetes patients newly registered during reporting period',
+    'At date', count(*)
+  FROM 	rpt_ic3_data_table
+  WHERE 	dmDx is NOT NULL
+         AND dmDxDate >= @startDate
+         AND ncdCurrentLocation=@location
+;
+
+/*
+	NCD-D3N - Diabetes patients currently enrolled patients that have ever experienced a complication -
+	Number of patients with a chronic care diagnosis of "Type 1 diabetes" or "Type 2 diabetes"
+	who have an "On treatment" status for the chronic care program at the facility on report end date,
+	who have ever had any of the complications in the "DIABETES HYPERTENSION INITIAL"
+	recorded in the past (Cardovascular disease, retinopathy, renal disease, stroke/TIA, PVD, Neuropathy, Sexual dysfunction)
+*/
+DELETE from rpt_ic3_indicators WHERE indicator = 'NCD-D3N';
+INSERT INTO rpt_ic3_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'NCD-D3N', 'Diabetes patients currently enrolled patients that have ever experienced a complication',
+    'At date', count(*)
+  FROM rpt_ic3_data_table r, mw_diabetes_hypertension_initial h
+  WHERE r.patient_id=h.patient_id
+        AND r.dmDx='X'
+        AND r.currentNcdState ='On treatment'
+        AND r.ncdCurrentLocation=@location
+        AND (h.cardiovascular_disease is not null
+             OR h.retinopathy is not null
+             OR h.renal_disease is not null
+             OR h.stroke_and_tia is not null
+             OR h.peripheral_vascular_disease is not null
+             OR h.neuropathy is not null
+             OR h.sexual_disorder is not null
+        )
+;
+
+/*
+	NCD-D4N - Diabetes Type 1 patients enrolled and active in care
+	Number of patients with a chronic care diagnosis of "Type 1 diabetes" who have an "On treatment" status
+	for the chronic care program at the facility on report end date
+*/
+DELETE from rpt_ic3_indicators WHERE indicator = 'NCD-D4N';
+INSERT INTO rpt_ic3_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'NCD-D4N', 'Diabetes Type 1 patients enrolled and active in care', 'At date', count(*)
+  FROM 	rpt_ic3_data_table
+  WHERE 	currentNcdState = "On treatment"
+         AND dmDx1 is NOT NULL
+         AND ncdCurrentLocation=@location
+;
+
+/*
+	NCD-D5N - Diabetes Type 1 patients newly registered during reporting period
+	Patients with a new chronic care diagnosis of "Type 1 Diabetes" in the last 3 months up to end date for report
+*/
+DELETE from rpt_ic3_indicators WHERE indicator = 'NCD-D5N';
+INSERT INTO rpt_ic3_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'NCD-D5N', 'Diabetes Type 1 patients newly registered during reporting period',
+    'At date', count(*)
+  FROM 	rpt_ic3_data_table
+  WHERE 	dmDx1 is NOT NULL
+         AND dmDxDate >= @startDate
+         AND ncdCurrentLocation=@location
+;
+
+/*
+	NCD-D6N - Diabetes Type 1 patients who have defaulted during the reporting period
+	"Type 1 diabetes" diagnosis patients whose last given appointment date exceeded 2 months / 8 weeks within the last 3 months up to the report
+	end date but did not have the outcomes (transferred out, died, treatment stopped) in the period
+*/
+DELETE from rpt_ic3_indicators WHERE indicator = 'NCD-D6N';
+INSERT INTO rpt_ic3_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'NCD-D6N', 'Diabetes Type 1 patients who have defaulted during the reporting period',
+    'At date', count(*)
+  FROM rpt_ic3_data_table
+  WHERE currentNcdState not in ('Patient transferred out', 'Patient died', 'Treatment stopped')
+        AND dmDx1 is not null
+        AND @startDate <= DATE_ADD(nextHtnDmAppt,INTERVAL +56 DAY) and DATE_ADD(nextHtnDmAppt,INTERVAL +56 DAY) <= @endDate
+        AND ncdCurrentLocation=@location
+;
+
+/*
+	NCD-D7N - Diabetes Type 2 patients enrolled and active in care
+	Number of patients with a chronic care diagnosis of "Type 2 diabetes" who have an "On treatment" status
+	for the chronic care program at the facility on report end date
+*/
+DELETE from rpt_ic3_indicators WHERE indicator = 'NCD-D7N';
+INSERT INTO rpt_ic3_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'NCD-D7N', 'Diabetes Type 2 patients enrolled and active in care', 'At date', count(*)
+  FROM 	rpt_ic3_data_table
+  WHERE 	currentNcdState = "On treatment"
+         AND dmDx2 is NOT NULL
+         AND ncdCurrentLocation=@location
+;
+
+/*
+	NCD-D8N - Diabetes Type 2 patients newly registered during reporting period
+	Patients with a new chronic care diagnosis of "Type 2 Diabetes" in the last 3 months up to end date for report
+*/
+DELETE from rpt_ic3_indicators WHERE indicator = 'NCD-D8N';
+INSERT INTO rpt_ic3_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'NCD-D8N', 'Diabetes Type 2 patients newly registered during reporting period',
+    'At date', count(*)
+  FROM 	rpt_ic3_data_table
+  WHERE 	dmDx2 is NOT NULL
+         AND dmDxDate >= @startDate
+         AND ncdCurrentLocation=@location
+;
+
+/*
+	NCD-D9N - Diabetes Type 2 patients who have defaulted during the reporting period
+	"Type 2 diabetes" diagnosis patients whose last given appointment date exceeded 2 months / 8 weeks within the last 3 months up to the report
+	end date but did not have the outcomes (transferred out, died, treatment stopped) in the period
+*/
+DELETE from rpt_ic3_indicators WHERE indicator = 'NCD-D9N';
+INSERT INTO rpt_ic3_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'NCD-D9N', 'Diabetes Type 2 patients who have defaulted during the reporting period',
+    'At date', count(*)
+  FROM rpt_ic3_data_table
+  WHERE currentNcdState not in ('Patient transferred out', 'Patient died', 'Treatment stopped')
+        AND dmDx2 is not null
+        AND @startDate <= DATE_ADD(nextHtnDmAppt,INTERVAL +56 DAY) and DATE_ADD(nextHtnDmAppt,INTERVAL +56 DAY) <= @endDate
+        AND ncdCurrentLocation=@location
+;
+
+/*
+	NCD-D10N - Type 1 patients with visit in last 3 months
+	"Type 1 diabetes" diagnosis patients with a visit in the last 3 months up to reporting end date
+*/
+DELETE from rpt_ic3_indicators WHERE indicator = 'NCD-D10N';
+INSERT INTO rpt_ic3_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'NCD-D10N', 'Type 1 patients with visit in last 3 months', 'At date', count(*)
+  FROM rpt_ic3_data_table
+  WHERE dmDx1 is not null
+        AND lastHtnDmVisitDate is not null
+        AND lastHtnDmVisitDate >= @startDate
+        AND ncdCurrentLocation=@location
+;
+
+/*
+	NCD-D11N - Type 2 patients with visit in last 3 months
+	"Type 2 diabetes" diagnosis patients with a visit in the last 3 months up to reporting end date
+*/
+DELETE from rpt_ic3_indicators WHERE indicator = 'NCD-D11N';
+INSERT INTO rpt_ic3_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'NCD-D11N', 'Type 2 patients with visit in last 3 months', 'At date', count(*)
+  FROM rpt_ic3_data_table
+  WHERE dmDx2 is not null
+        AND lastHtnDmVisitDate is not null
+        AND lastHtnDmVisitDate >= @startDate
+        AND ncdCurrentLocation=@location
+;
+
+/*
+	NCD-D12N - Type 1 patients with a visit in the last three months, with FBS ( <=120 mg/dL)
+	"Type 1 diabetes" diagnosis patients with a visit in the last 3 months up to reporting end date
+	and a fingerstick recording at most recent visit with a value of <120 (concept : 887)
+*/
+DELETE from rpt_ic3_indicators WHERE indicator = 'NCD-D12N';
+INSERT INTO rpt_ic3_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'NCD-D12N', 'Type 1 patients with a visit in the last three months, with FBS (<=120 mg/dL)', 'At date', count(*)
+  FROM rpt_ic3_data_table
+  WHERE dmDx1 is not null
+        AND lastHtnDmVisitDate is not null
+        AND lastHtnDmVisitDate >= @startDate
+        AND fingerStickAtLastVisit is not null
+        AND fingerStickAtLastVisit <=120
+        AND ncdCurrentLocation=@location
+;
+
+/*
+	NCD-D13N - Type 2 patients with a visit in the last three months, with FBS (<=120 mg/dL)
+	"Type 2 diabetes" diagnosis patients with a visit in the last 3 months up to reporting end date
+	and a fingerstick recording at most recent visit with a value of <120 (concept : 887)
+*/
+DELETE from rpt_ic3_indicators WHERE indicator = 'NCD-D13N';
+INSERT INTO rpt_ic3_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'NCD-D13N', 'Type 2 patients with a visit in the last three months, with FBS (<=120 mg/dL)', 'At date', count(*)
+  FROM rpt_ic3_data_table
+  WHERE dmDx2 is not null
+        AND lastHtnDmVisitDate is not null
+        AND lastHtnDmVisitDate >= @startDate
+        AND fingerStickAtLastVisit is not null
+        AND fingerStickAtLastVisit <=120
+        AND ncdCurrentLocation=@location
+;
+
+/*
+	NCD-D14N - Type 2 patients on [long-acting or short-acting] Insulin
+	"Type 2 diabetes" diagnosis patients with a recording of long acting
+	or short acting insulin at most recent visit (concept : 1193)
+	and answers (concept: 6750) OR (concept : 282)
+*/
+DELETE from rpt_ic3_indicators WHERE indicator = 'NCD-D14N';
+INSERT INTO rpt_ic3_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'NCD-D14N', 'Type 2 patients on [long-acting or short-acting] Insulin', 'At date', count(*)
+  FROM rpt_ic3_data_table
+  WHERE dmDx2 is not null
+        AND lastHtnDmVisitDate is not null
+        AND lastHtnDmVisitDate >= @startDate
+        AND (longActingInsulin is not null or shortActingInsulin is not null)
+        AND ncdCurrentLocation=@location
 ;
 
 select * from rpt_ic3_indicators;
