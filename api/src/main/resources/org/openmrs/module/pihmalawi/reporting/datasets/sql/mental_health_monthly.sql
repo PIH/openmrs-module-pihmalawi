bc4ce73c6f9db43dd95279e791eab7e514c36bb1
Cosmin Ioan <cioan@pih.org>
2022-03-07 09:29:39 -0500

    MLW-1312, add alcohol and drug use disorders

--- api/src/main/resources/org/openmrs/module/pihmalawi/reporting/datasets/sql/mental_health_monthly.sql
+++ api/src/main/resources/org/openmrs/module/pihmalawi/reporting/datasets/sql/mental_health_monthly.sql
@@ -159,4 +159,413 @@ INSERT INTO rpt_mh_indicators
          and  ageAtLastVisit >= 16
 ;
 
+
+/*
+	OMDA015_M_NC - Number of Male patients under 15 patients with new cases of ACUTE organic mental disorder
+	at the facility on report end date
+*/
+DELETE from rpt_mh_indicators WHERE indicator = 'OMDA015_M_NC';
+INSERT INTO rpt_mh_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'OMDA015_M_NC', 'New Male cases under 15 yo patients with ACUTE organic mental disorder', 'At date', count(*)
+  FROM 	rpt_mh_data_table
+  WHERE 	dx_organic_mental_disorder_acute is not null
+         and mhIntakeVisitDate> @startDate
+         and  mhIntakeVisitDate <@endDate
+         and mhIntakeLocation = @location
+         and gender= 'M'
+         and  ageAtIntake < 16
+;
+
+/*
+	OMDA16_M_NC - Number of Male patients over 16 yo patients with new cases of ACUTE organic mental disorder
+	at the facility on report end date
+*/
+DELETE from rpt_mh_indicators WHERE indicator = 'OMDA16_M_NC';
+INSERT INTO rpt_mh_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'OMDA16_M_NC', 'New Male cases over 16 yo patients with ACUTE organic mental disorder', 'At date', count(*)
+  FROM 	rpt_mh_data_table
+  WHERE 	dx_organic_mental_disorder_acute is not null
+         and mhIntakeVisitDate> @startDate
+         and  mhIntakeVisitDate <@endDate
+         and mhIntakeLocation = @location
+         and gender= 'M'
+         and  ageAtIntake >= 16
+;
+
+/*
+	OMDA015_F_NC - Number of Female patients under 15 yo patients with new cases of ACUTE organic mental disorder
+	at the facility on report end date
+*/
+DELETE from rpt_mh_indicators WHERE indicator = 'OMDA015_F_NC';
+INSERT INTO rpt_mh_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'OMDA015_F_NC', 'New Female cases under 15 yo patients with ACUTE organic mental disorder', 'At date', count(*)
+  FROM 	rpt_mh_data_table
+  WHERE 	dx_organic_mental_disorder_acute is not null
+         and mhIntakeVisitDate> @startDate
+         and  mhIntakeVisitDate <@endDate
+         and mhIntakeLocation = @location
+         and gender= 'F'
+         and  ageAtIntake < 16
+;
+
+/*
+	OMDA16_F_NC - Number of Female patients over 16 yo patients with new cases of ACUTE organic mental disorder
+	at the facility on report end date
+*/
+DELETE from rpt_mh_indicators WHERE indicator = 'OMDA16_F_NC';
+INSERT INTO rpt_mh_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'OMDA16_F_NC', 'New Female cases over 16 yo patients with ACUTE organic mental disorder', 'At date', count(*)
+  FROM 	rpt_mh_data_table
+  WHERE 	dx_organic_mental_disorder_acute is not null
+         and mhIntakeVisitDate> @startDate
+         and  mhIntakeVisitDate <@endDate
+         and mhIntakeLocation = @location
+         and gender= 'F'
+         and  ageAtIntake >= 16
+;
+
+/*
+	OMDA015_M_SC - Number of Male patients under 15 with a ACUTE organic mental disorder dx
+	who had a visit (subsequent case) at the facility within the reporting period
+*/
+DELETE from rpt_mh_indicators WHERE indicator = 'OMDA015_M_SC';
+INSERT INTO rpt_mh_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'OMDA015_M_SC', 'Number of Male patients under 15 with a ACUTE organic mental disorder dx who had a visit', 'At date', count(*)
+  FROM 	rpt_mh_data_table
+  WHERE 	dx_organic_mental_disorder_acute is not null
+         and lastMHVisitDate > @startDate
+         and  lastMHVisitDate < @endDate
+         and visitLocation = @location
+         and gender= 'M'
+         and  ageAtLastVisit < 16
+;
+
+/*
+	OMDA16_M_SC - Number of Male patients over 16 with a ACUTE organic mental disorder dx
+	who had a visit (subsequent case) at the facility within the reporting period
+*/
+DELETE from rpt_mh_indicators WHERE indicator = 'OMDA16_M_SC';
+INSERT INTO rpt_mh_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'OMDA16_M_SC', 'Number of Male patients over 16 with a ACUTE organic mental disorder dx who had a visit', 'At date', count(*)
+  FROM 	rpt_mh_data_table
+  WHERE 	dx_organic_mental_disorder_acute is not null
+         and lastMHVisitDate > @startDate
+         and  lastMHVisitDate < @endDate
+         and visitLocation = @location
+         and gender= 'M'
+         and  ageAtLastVisit >= 16
+;
+
+/*
+	OMDA015_F_SC - Number of Female patients under 15 with a ACUTE organic mental disorder dx
+	who had a visit (subsequent case) at the facility within the reporting period
+*/
+DELETE from rpt_mh_indicators WHERE indicator = 'OMDA015_F_SC';
+INSERT INTO rpt_mh_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'OMDA015_F_SC', 'Number of Female patients under 15 with a ACUTE organic mental disorder dx who had a visit', 'At date', count(*)
+  FROM 	rpt_mh_data_table
+  WHERE 	dx_organic_mental_disorder_acute is not null
+         and lastMHVisitDate > @startDate
+         and  lastMHVisitDate < @endDate
+         and visitLocation = @location
+         and gender= 'F'
+         and  ageAtLastVisit < 16
+;
+
+/*
+	OMDA16_F_SC - Number of Female patients over 16 with a ACUTE organic mental disorder dx
+	who had a visit (subsequent case) at the facility within the reporting period
+*/
+DELETE from rpt_mh_indicators WHERE indicator = 'OMDA16_F_SC';
+INSERT INTO rpt_mh_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'OMDA16_F_SC', 'Number of Female patients over 16 with a ACUTE organic mental disorder dx who had a visit', 'At date', count(*)
+  FROM 	rpt_mh_data_table
+  WHERE 	dx_organic_mental_disorder_acute is not null
+         and lastMHVisitDate > @startDate
+         and  lastMHVisitDate < @endDate
+         and visitLocation = @location
+         and gender= 'F'
+         and  ageAtLastVisit >= 16
+;
+
+/*
+	AUMD015_M_NC - Number of Male patients under 15 patients with new cases of Alcohol use mental disorder
+	at the facility on report end date
+*/
+DELETE from rpt_mh_indicators WHERE indicator = 'AUMD015_M_NC';
+INSERT INTO rpt_mh_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'AUMD015_M_NC', 'New Male cases under 15 yo patients with Alcohol use mental disorder', 'At date', count(*)
+  FROM 	rpt_mh_data_table
+  WHERE 	dx_alcohol_use_mental_disorder is not null
+         and mhIntakeVisitDate> @startDate
+         and  mhIntakeVisitDate <@endDate
+         and mhIntakeLocation = @location
+         and gender= 'M'
+         and  ageAtIntake < 16
+;
+
+/*
+	AUMD16_M_NC - Number of Male patients over 16 yo patients with new cases of Alcohol use mental disorder
+	at the facility on report end date
+*/
+DELETE from rpt_mh_indicators WHERE indicator = 'AUMD16_M_NC';
+INSERT INTO rpt_mh_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'AUMD16_M_NC', 'New Male cases over 16 yo patients with Alcohol use mental disorder', 'At date', count(*)
+  FROM 	rpt_mh_data_table
+  WHERE 	dx_alcohol_use_mental_disorder is not null
+         and mhIntakeVisitDate> @startDate
+         and  mhIntakeVisitDate <@endDate
+         and mhIntakeLocation = @location
+         and gender= 'M'
+         and  ageAtIntake >= 16
+;
+
+/*
+	AUMD015_F_NC - Number of Female patients under 15 yo patients with new cases of Alcohol use mental disorder
+	at the facility on report end date
+*/
+DELETE from rpt_mh_indicators WHERE indicator = 'AUMD015_F_NC';
+INSERT INTO rpt_mh_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'AUMD015_F_NC', 'New Female cases under 15 yo patients with Alcohol use mental disorder', 'At date', count(*)
+  FROM 	rpt_mh_data_table
+  WHERE 	dx_alcohol_use_mental_disorder is not null
+         and mhIntakeVisitDate> @startDate
+         and  mhIntakeVisitDate <@endDate
+         and mhIntakeLocation = @location
+         and gender= 'F'
+         and  ageAtIntake < 16
+;
+
+/*
+	AUMD16_F_NC - Number of Female patients over 16 yo patients with new cases of Alcohol use mental disorder
+	at the facility on report end date
+*/
+DELETE from rpt_mh_indicators WHERE indicator = 'AUMD16_F_NC';
+INSERT INTO rpt_mh_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'AUMD16_F_NC', 'New Female cases over 16 yo patients with Alcohol use mental disorder', 'At date', count(*)
+  FROM 	rpt_mh_data_table
+  WHERE 	dx_alcohol_use_mental_disorder is not null
+         and mhIntakeVisitDate> @startDate
+         and  mhIntakeVisitDate <@endDate
+         and mhIntakeLocation = @location
+         and gender= 'F'
+         and  ageAtIntake >= 16
+;
+
+/*
+	AUMD015_M_SC - Number of Male patients under 15 with a Alcohol use mental disorder dx
+	who had a visit (subsequent case) at the facility within the reporting period
+*/
+DELETE from rpt_mh_indicators WHERE indicator = 'AUMD015_M_SC';
+INSERT INTO rpt_mh_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'AUMD015_M_SC', 'Number of Male patients under 15 with a Alcohol use mental disorder dx who had a visit', 'At date', count(*)
+  FROM 	rpt_mh_data_table
+  WHERE 	dx_alcohol_use_mental_disorder is not null
+         and lastMHVisitDate > @startDate
+         and  lastMHVisitDate < @endDate
+         and visitLocation = @location
+         and gender= 'M'
+         and  ageAtLastVisit < 16
+;
+
+/*
+	AUMD16_M_SC - Number of Male patients over 16 with a Alcohol use mental disorder dx
+	who had a visit (subsequent case) at the facility within the reporting period
+*/
+DELETE from rpt_mh_indicators WHERE indicator = 'AUMD16_M_SC';
+INSERT INTO rpt_mh_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'AUMD16_M_SC', 'Number of Male patients over 16 with a Alcohol use mental disorder dx who had a visit', 'At date', count(*)
+  FROM 	rpt_mh_data_table
+  WHERE 	dx_alcohol_use_mental_disorder is not null
+         and lastMHVisitDate > @startDate
+         and  lastMHVisitDate < @endDate
+         and visitLocation = @location
+         and gender= 'M'
+         and  ageAtLastVisit >= 16
+;
+
+/*
+	AUMD015_F_SC - Number of Female patients under 15 with a Alcohol use mental disorder dx
+	who had a visit (subsequent case) at the facility within the reporting period
+*/
+DELETE from rpt_mh_indicators WHERE indicator = 'AUMD015_F_SC';
+INSERT INTO rpt_mh_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'AUMD015_F_SC', 'Number of Female patients under 15 with a Alcohol use mental disorder dx who had a visit', 'At date', count(*)
+  FROM 	rpt_mh_data_table
+  WHERE 	dx_alcohol_use_mental_disorder is not null
+         and lastMHVisitDate > @startDate
+         and  lastMHVisitDate < @endDate
+         and visitLocation = @location
+         and gender= 'F'
+         and  ageAtLastVisit < 16
+;
+
+/*
+	AUMD16_F_SC - Number of Female patients over 16 with a Alcohol use mental disorder dx
+	who had a visit (subsequent case) at the facility within the reporting period
+*/
+DELETE from rpt_mh_indicators WHERE indicator = 'AUMD16_F_SC';
+INSERT INTO rpt_mh_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'AUMD16_F_SC', 'Number of Female patients over 16 with a Alcohol use mental disorder dx who had a visit', 'At date', count(*)
+  FROM 	rpt_mh_data_table
+  WHERE 	dx_alcohol_use_mental_disorder is not null
+         and lastMHVisitDate > @startDate
+         and  lastMHVisitDate < @endDate
+         and visitLocation = @location
+         and gender= 'F'
+         and  ageAtLastVisit >= 16
+;
+
+/*
+	DUMD015_M_NC - Number of Male patients under 15 patients with new cases of Drug use mental disorder
+	at the facility on report end date
+*/
+DELETE from rpt_mh_indicators WHERE indicator = 'DUMD015_M_NC';
+INSERT INTO rpt_mh_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'DUMD015_M_NC', 'New Male cases under 15 yo patients with Drug use mental disorder', 'At date', count(*)
+  FROM 	rpt_mh_data_table
+  WHERE 	dx_drug_use_mental_disorder is not null
+         and mhIntakeVisitDate> @startDate
+         and  mhIntakeVisitDate <@endDate
+         and mhIntakeLocation = @location
+         and gender= 'M'
+         and  ageAtIntake < 16
+;
+
+/*
+	DUMD16_M_NC - Number of Male patients over 16 yo patients with new cases of Drug use mental disorder
+	at the facility on report end date
+*/
+DELETE from rpt_mh_indicators WHERE indicator = 'DUMD16_M_NC';
+INSERT INTO rpt_mh_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'DUMD16_M_NC', 'New Male cases over 16 yo patients with Drug use mental disorder', 'At date', count(*)
+  FROM 	rpt_mh_data_table
+  WHERE 	dx_drug_use_mental_disorder is not null
+         and mhIntakeVisitDate> @startDate
+         and  mhIntakeVisitDate <@endDate
+         and mhIntakeLocation = @location
+         and gender= 'M'
+         and  ageAtIntake >= 16
+;
+
+/*
+	DUMD015_F_NC - Number of Female patients under 15 yo patients with new cases of Drug use mental disorder
+	at the facility on report end date
+*/
+DELETE from rpt_mh_indicators WHERE indicator = 'DUMD015_F_NC';
+INSERT INTO rpt_mh_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'DUMD015_F_NC', 'New Female cases under 15 yo patients with Drug use mental disorder', 'At date', count(*)
+  FROM 	rpt_mh_data_table
+  WHERE 	dx_drug_use_mental_disorder is not null
+         and mhIntakeVisitDate> @startDate
+         and  mhIntakeVisitDate <@endDate
+         and mhIntakeLocation = @location
+         and gender= 'F'
+         and  ageAtIntake < 16
+;
+
+/*
+	DUMD16_F_NC - Number of Female patients over 16 yo patients with new cases of Drug use mental disorder
+	at the facility on report end date
+*/
+DELETE from rpt_mh_indicators WHERE indicator = 'DUMD16_F_NC';
+INSERT INTO rpt_mh_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'DUMD16_F_NC', 'New Female cases over 16 yo patients with Drug use mental disorder', 'At date', count(*)
+  FROM 	rpt_mh_data_table
+  WHERE 	dx_drug_use_mental_disorder is not null
+         and mhIntakeVisitDate> @startDate
+         and  mhIntakeVisitDate <@endDate
+         and mhIntakeLocation = @location
+         and gender= 'F'
+         and  ageAtIntake >= 16
+;
+
+/*
+	DUMD015_M_SC - Number of Male patients under 15 with a Drug use mental disorder dx
+	who had a visit (subsequent case) at the facility within the reporting period
+*/
+DELETE from rpt_mh_indicators WHERE indicator = 'DUMD015_M_SC';
+INSERT INTO rpt_mh_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'DUMD015_M_SC', 'Number of Male patients under 15 with a Drug use mental disorder dx who had a visit', 'At date', count(*)
+  FROM 	rpt_mh_data_table
+  WHERE 	dx_drug_use_mental_disorder is not null
+         and lastMHVisitDate > @startDate
+         and  lastMHVisitDate < @endDate
+         and visitLocation = @location
+         and gender= 'M'
+         and  ageAtLastVisit < 16
+;
+
+/*
+	DUMD16_M_SC - Number of Male patients over 16 with a Drug use mental disorder dx
+	who had a visit (subsequent case) at the facility within the reporting period
+*/
+DELETE from rpt_mh_indicators WHERE indicator = 'DUMD16_M_SC';
+INSERT INTO rpt_mh_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'DUMD16_M_SC', 'Number of Male patients over 16 with a Drug use mental disorder dx who had a visit', 'At date', count(*)
+  FROM 	rpt_mh_data_table
+  WHERE 	dx_drug_use_mental_disorder is not null
+         and lastMHVisitDate > @startDate
+         and  lastMHVisitDate < @endDate
+         and visitLocation = @location
+         and gender= 'M'
+         and  ageAtLastVisit >= 16
+;
+
+/*
+	DUMD015_F_SC - Number of Female patients under 15 with a Drug use mental disorder dx
+	who had a visit (subsequent case) at the facility within the reporting period
+*/
+DELETE from rpt_mh_indicators WHERE indicator = 'DUMD015_F_SC';
+INSERT INTO rpt_mh_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'DUMD015_F_SC', 'Number of Female patients under 15 with a Drug use mental disorder dx who had a visit', 'At date', count(*)
+  FROM 	rpt_mh_data_table
+  WHERE 	dx_drug_use_mental_disorder is not null
+         and lastMHVisitDate > @startDate
+         and  lastMHVisitDate < @endDate
+         and visitLocation = @location
+         and gender= 'F'
+         and  ageAtLastVisit < 16
+;
+
+/*
+	DUMD16_F_SC - Number of Female patients over 16 with a Drug use mental disorder dx
+	who had a visit (subsequent case) at the facility within the reporting period
+*/
+DELETE from rpt_mh_indicators WHERE indicator = 'DUMD16_F_SC';
+INSERT INTO rpt_mh_indicators
+(indicator, description, indicator_type, indicator_value)
+  SELECT 'DUMD16_F_SC', 'Number of Female patients over 16 with a Drug use mental disorder dx who had a visit', 'At date', count(*)
+  FROM 	rpt_mh_data_table
+  WHERE 	dx_drug_use_mental_disorder is not null
+         and lastMHVisitDate > @startDate
+         and  lastMHVisitDate < @endDate
+         and visitLocation = @location
+         and gender= 'F'
+         and  ageAtLastVisit >= 16
+;
+
 select * from rpt_mh_indicators;
