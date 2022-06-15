b03961fad57af8ce5f62d67dc347a3451e07296f
kmatiya <khormatiya@gmail.com>
2022-06-15 15:50:52 +0200

    MLW-1349 Add MoH Cohort Disaggregated report

---
+++ api/src/main/resources/org/openmrs/module/pihmalawi/reporting/datasets/sql/moh-cohort-disaggregated.sql
@@ -0,0 +1,74 @@
+/************************************************************************
+
+MOH COHORT DISAGGREGATED REPORT
+Use OpenMRS data warehouse tables and stored procedures
+
+Query aggregates active patients on ART.
+Patients whose last appointment exceeds 60 days without visiting a heath facility
+are considered to be defaulters
+*************************************************************************/
+
+/* 30 for age in months*/
+SET @birthDateDivider = 30;
+SET @defaultCutOff = 60;
+
+DROP TEMPORARY TABLE IF EXISTS moh_cohort_disaggregated;
+     CREATE TEMPORARY TABLE moh_cohort_disaggregated(
+         sort_value INT PRIMARY KEY auto_increment,
+         age_group VARCHAR(100),
+         gender varchar(10),
+         tx_new int,
+         tx_curr int,
+         tx_curr_ipt int,
+         tx_curr_screened_tb int,
+         0A int,
+         1A int,
+         2A int,
+         3A int,
+         4A int,
+         5A int,
+         6A int,
+         7A int,
+         8A int,
+         9A int,
+         10A int,
+         11A int,
+         12A int,
+         13A int,
+         14A int,
+         15A int,
+         16A int,
+         17A int,
+         0P int,
+         1P int,
+         2P int,
+         3P int,
+         4P int,
+         4PP int,
+         4PA int,
+         9P int,
+         9PP int,
+         11P int,
+         11PP int,
+         11PA int,
+         12PP int,
+         12PA int,
+         14PP int,
+         14PA int,
+         15P int,
+         15PP int,
+         15PA int,
+         16P int,
+         17P int,
+         17PP int,
+         17PA int,
+         non_standard int
+     );
+
+call create_moh_cohort_disaggregated(@startDate,@endDate,@location,@defaultCutOff,@birthDateDivider);
+call create_moh_male_aggregation(@startDate,@endDate,@location,@defaultCutOff,@birthDateDivider);
+call create_moh_female_maternal_aggregation(@startDate,@endDate,@location,@defaultCutOff,@birthDateDivider,"Patient Pregnant","FP");
+call create_moh_fnp_aggregation(@startDate,@endDate,@location,@defaultCutOff,@birthDateDivider,"FNP");
+call create_moh_female_maternal_aggregation(@startDate,@endDate,@location,@defaultCutOff,@birthDateDivider,"Currently breastfeeding child","Fbf");
+
+select * from moh_cohort_disaggregated;
