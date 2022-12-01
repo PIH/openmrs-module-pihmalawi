fc78e191423979c5882b6829fe48eea014981b11
Cosmin Ioan <cioan@pih.org>
2022-12-01 17:51:29 -0500

    MLW-1402, add location and user to the duplicate encounter report

--- api/src/main/resources/org/openmrs/module/pihmalawi/reporting/reports/sql/duplicateEncounters.sql
+++ api/src/main/resources/org/openmrs/module/pihmalawi/reporting/reports/sql/duplicateEncounters.sql
@@ -4,7 +4,7 @@
 -- ## report_description = Report listing patients with duplicate encounters(same encounter type on the same day)  (revision: November 2022)
 -- ## parameter = startDate|Start Date|java.util.Date
 -- ## parameter = endDate|End Date|java.util.Date
-
+-- ## parameter = location|Location|org.openmrs.Location
 
 # SET @startDate = '2022-01-01';
 # SET @endDate = '2022-11-29';
@@ -14,9 +14,18 @@ select distinct a.patient_id as 'PID',
                 a.encounter_type as 'EncounterTypeId',
                 t.name as 'EncounterType',
                 a.encounter_datetime as 'EncounterDateTime',
+                a.date_created as 'DateCreated',
+                CONCAT_WS(' ', n.given_name, n.family_name ) as 'User',
+                loc.name as 'Location',
                 a.voided as 'Voided'
 from encounter a
+  inner join users u on a.creator = u.user_id
+  inner join person_name n on u.person_id = n.person_id
   inner join encounter_type t on a.encounter_type = t.encounter_type_id
+  inner join location loc on a.location_id = loc.location_id
   inner join encounter b on a.patient_id=b.patient_id and a.encounter_type=b.encounter_type and a.encounter_datetime = b.encounter_datetime
-where a.encounter_id <> b.encounter_id and a.voided=0 and b.voided = 0 and a.encounter_datetime > @startDate and a.encounter_datetime < @endDate
+where a.encounter_id <> b.encounter_id and a.voided=0 and b.voided = 0
+      and a.location_id = @location
+      and a.encounter_datetime > @startDate
+      and a.encounter_datetime < @endDate
 order by a.patient_id desc, a.encounter_datetime desc;
