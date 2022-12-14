b435ee540c39f270ebabe2d6916c2a1a78771309
Cosmin Ioan <cioan@pih.org>
2022-12-14 07:03:15 -0500

    MLW-1415, skip over the already voided encounters

--- api/src/main/java/org/openmrs/module/pihmalawi/patient/impl/ExtendedEncounterServiceImpl.java
+++ api/src/main/java/org/openmrs/module/pihmalawi/patient/impl/ExtendedEncounterServiceImpl.java
@@ -60,42 +60,44 @@ public class ExtendedEncounterServiceImpl extends BaseOpenmrsService implements 
         List<Encounter> deletedEncounters = new ArrayList<>();
         EncounterService encounterService = Context.getEncounterService();
         for (Encounter enc : duplicates) {
-            Set<Obs> encObs = enc.getObs();
-            EncounterSearchCriteria criteria = new EncounterSearchCriteriaBuilder()
-                    .setPatient(enc.getPatient())
-                    .setEncounterTypes(Arrays.asList(enc.getEncounterType()))
-                    .setFromDate(enc.getEncounterDatetime())
-                    .setToDate(enc.getEncounterDatetime())
-                    .setLocation(enc.getLocation())
-                    .setIncludeVoided(false)
-                    .createEncounterSearchCriteria();
-            List<Encounter> encounters = encounterService.getEncounters(criteria);
-            for (Encounter possibleDup : encounters) {
-                if (possibleDup.getId().compareTo(enc.getId()) != 0) {
-                    //compare the obs
-                    Set<Obs> possibleDupObs = possibleDup.getObs();
-                    boolean exactMatch =  true;
-                    for (Obs possibleDupOb : possibleDupObs) {
-                        Concept concept = possibleDupOb.getConcept();
-                        String valueAsString = possibleDupOb.getValueAsString(Context.getLocale());
-                        Obs foundObs = encObs.stream()
-                                .filter(obs -> concept.getUuid().equals(obs.getConcept().getUuid()) && valueAsString.equals(obs.getValueAsString(Context.getLocale())))
-                                .findAny()
-                                .orElse(null);
-                        if (foundObs == null ) {
-                            //we did not find an exact match Obs
-                            exactMatch = false;
-                            break;
+            if (!enc.getVoided()) {
+                Set<Obs> encObs = enc.getObs();
+                EncounterSearchCriteria criteria = new EncounterSearchCriteriaBuilder()
+                        .setPatient(enc.getPatient())
+                        .setEncounterTypes(Arrays.asList(enc.getEncounterType()))
+                        .setFromDate(enc.getEncounterDatetime())
+                        .setToDate(enc.getEncounterDatetime())
+                        .setLocation(enc.getLocation())
+                        .setIncludeVoided(false)
+                        .createEncounterSearchCriteria();
+                List<Encounter> encounters = encounterService.getEncounters(criteria);
+                for (Encounter possibleDup : encounters) {
+                    if (possibleDup.getId().compareTo(enc.getId()) != 0) {
+                        //compare the obs
+                        Set<Obs> possibleDupObs = possibleDup.getObs();
+                        boolean exactMatch = true;
+                        for (Obs possibleDupOb : possibleDupObs) {
+                            Concept concept = possibleDupOb.getConcept();
+                            String valueAsString = possibleDupOb.getValueAsString(Context.getLocale());
+                            Obs foundObs = encObs.stream()
+                                    .filter(obs -> concept.getUuid().equals(obs.getConcept().getUuid()) && valueAsString.equals(obs.getValueAsString(Context.getLocale())))
+                                    .findAny()
+                                    .orElse(null);
+                            if (foundObs == null) {
+                                //we did not find an exact match Obs
+                                exactMatch = false;
+                                break;
+                            }
+
+                        }
+                        if (exactMatch && possibleDupObs.size() == encObs.size()) {
+                            // this is a duplicate Encounter with the same Obs values
+                            //delete the Encounter
+                            Encounter deletedEncounter = encounterService.voidEncounter(possibleDup, "system deleting duplicate encounters");
+                            deletedEncounters.add(deletedEncounter);
                         }
 
                     }
-                    if (exactMatch && possibleDupObs.size() == encObs.size()) {
-                        // this is a duplicate Encounter with the same Obs values
-                        //delete the Encounter
-                        Encounter deletedEncounter = encounterService.voidEncounter(possibleDup, "system deleting duplicate encounters");
-                        deletedEncounters.add(deletedEncounter);
-                    }
-
                 }
             }
         }
