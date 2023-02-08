9e694e2b2abe17115b578c4195265c4c3f6829f2
Cosmin Ioan <cioan@pih.org>
2023-02-08 11:21:43 -0500

    MLW-1413, merge in the 7.x branch

--- api/src/main/java/org/openmrs/module/pihmalawi/activator/PihMalawiModuleActivator.java
+++ api/src/main/java/org/openmrs/module/pihmalawi/activator/PihMalawiModuleActivator.java
@@ -65,6 +65,7 @@ public class PihMalawiModuleActivator extends BaseModuleActivator implements Dae
 		l.add(new AddressTemplateInitializer());
 		l.add(new HtmlFormInitializer());
 		l.add(new ReportInitializer());
+        l.add(new AuthenticationInitializer());
 		return l;
 	}
 
