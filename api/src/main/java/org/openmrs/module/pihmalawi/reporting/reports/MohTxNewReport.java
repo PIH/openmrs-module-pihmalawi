5d74b45b47873a37a66d9e25475795a2491ecae9
b-ngongonda <brightngongonda@gmail.com>
2024-01-11 11:58:37 +0200

    MLW-1572: Add MOH Tx New Report

---
+++ api/src/main/java/org/openmrs/module/pihmalawi/reporting/reports/MohTxNewReport.java
@@ -0,0 +1,88 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.pihmalawi.reporting.reports;
+
+import org.openmrs.module.pihmalawi.PihMalawiConstants;
+import org.openmrs.module.pihmalawi.reporting.definition.dataset.definition.SqlFileDataSetDefinition;
+import org.openmrs.module.reporting.ReportingConstants;
+import org.openmrs.module.reporting.evaluation.parameter.Mapped;
+import org.openmrs.module.reporting.evaluation.parameter.Parameter;
+import org.openmrs.module.reporting.report.ReportDesign;
+import org.openmrs.module.reporting.report.definition.ReportDefinition;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class MohTxNewReport extends ApzuReportManager {
+
+    public static final String SQL_DATA_SET_RESOURCE = "org/openmrs/module/pihmalawi/reporting/datasets/sql/moh-tx_new-report.sql";
+    public static final String EXCEL_REPORT_DESIGN_UUID = "6ed810f9-eaa3-4f6d-9f2d-0f57a7c40ab5";
+
+    @Override
+    public String getUuid() {
+        return "7d5fd0ff-8517-4077-a65f-cd626e9bb181";
+    }
+
+    @Override
+    public String getName() {
+        return "MOH - TX New Report";
+    }
+
+    @Override
+    public String getDescription() {
+        return "MOH TX New Report, Revision January 2024";
+    }
+
+    @Override
+    public List<Parameter> getParameters() {
+        List<Parameter> l = new ArrayList<Parameter>();
+        l.add(ReportingConstants.START_DATE_PARAMETER);
+        l.add(ReportingConstants.END_DATE_PARAMETER);
+        l.add(ReportingConstants.LOCATION_PARAMETER);
+        return l;
+    }
+
+    /**
+     * @see ApzuReportManager#constructReportDefinition()
+     */
+    @Override
+    public ReportDefinition constructReportDefinition() {
+
+        ReportDefinition rd = new ReportDefinition();
+        rd.setUuid(getUuid());
+        rd.setName(getName());
+        rd.setDescription(getDescription());
+        rd.setParameters(getParameters());
+
+        SqlFileDataSetDefinition dsd = new SqlFileDataSetDefinition();
+        dsd.setParameters(getParameters());
+        dsd.setConnectionPropertyFile(PihMalawiConstants.OPENMRS_WAREHOUSE_CONNECTION_PROPERTIES_FILE_NAME);
+        dsd.setSqlResource(SQL_DATA_SET_RESOURCE);
+
+        rd.addDataSetDefinition("mohTxNewReport", Mapped.mapStraightThrough(dsd));
+
+        return rd;
+    }
+
+    @Override
+    public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
+        List<ReportDesign> l = new ArrayList<ReportDesign>();
+        ReportDesign design = createExcelTemplateDesign(EXCEL_REPORT_DESIGN_UUID, reportDefinition, "MohTxNew.xls");
+        design.addPropertyValue("repeatingSections", "sheet:1,row:5,dataset: mohTxNewReport");
+        l.add(design);
+        return l;
+    }
+}
