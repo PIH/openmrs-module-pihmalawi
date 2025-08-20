ceaed5338fd9cd4f2ced94f3ad0437e848471007
Michael Seaton <mseaton@pih.org>
2025-08-20 14:13:56 -0400

    MLW-1665 - ART Register in SQL

--- configuration/configuration/liquibase/sql/patient_identifier_functions.sql
+++ configuration/configuration/liquibase/sql/patient_identifier_functions.sql
@@ -4,6 +4,37 @@
 -- DELIMITER #
 
 #
+DROP FUNCTION IF EXISTS format_identifier;
+#
+CREATE FUNCTION format_identifier(
+    _identifier varchar(255)
+)
+    RETURNS varchar(255)
+    DETERMINISTIC
+
+BEGIN
+    DECLARE first_dash int;
+    DECLARE second_dash int;
+    DECLARE second_length int;
+
+    SET _identifier = replace(upper(trim(_identifier)), ' ', '-');
+
+    if _identifier is null or _identifier = '' then
+        return null;
+    else
+        SET first_dash = locate('-', _identifier);
+        SET second_dash = locate('-', _identifier, first_dash + 1);
+        SET second_length = if(second_dash > 0, second_dash - 1, length(_identifier)) - first_dash;
+        RETURN concat(
+            substr(_identifier, 1, first_dash),
+            lpad(substr(_identifier, first_dash+1, second_length), 4, '0'),
+            if(second_length > 0, substr(_identifier, second_dash), '')
+        );
+    end if;
+END
+#
+
+#
 /*
     return the best patient identifier of the given type.
     This will not restrict by location, but will prefer identifiers at the given location
@@ -22,7 +53,7 @@ CREATE FUNCTION patient_identifier(
 BEGIN
     DECLARE ret varchar(50);
 
-    SELECT      i.identifier INTO ret
+    SELECT      format_identifier(i.identifier) INTO ret
     FROM        patient_identifier i
     WHERE       i.identifier_type = _identifier_type
       AND       i.voided = 0
@@ -51,7 +82,7 @@ CREATE FUNCTION patient_identifiers_of_type(
 BEGIN
     DECLARE ret text;
 
-    SELECT      group_concat(i.identifier separator ', ') INTO ret
+    SELECT      group_concat(format_identifier(i.identifier) separator ', ') INTO ret
     FROM        patient_identifier i
     WHERE       i.identifier_type = _identifier_type
       AND       i.voided = 0
