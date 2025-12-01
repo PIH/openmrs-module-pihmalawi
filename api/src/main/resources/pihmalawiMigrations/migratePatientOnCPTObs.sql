f9f58f2521d63763998294ed221d45d4bead65c5
Cosmin <cioan@pih.org>
2025-12-01 18:34:23 -0500

    MLW-1758: clean up onCPT coded answers stored as numeric values

--- api/src/main/resources/pihmalawiMigrations/migratePatientOnCPTObs.sql
+++ api/src/main/resources/pihmalawiMigrations/migratePatientOnCPTObs.sql
@@ -4,14 +4,14 @@ SET @yes_concept_id = (select concept_id from concept where uuid = '65576354-977
 SET @no_concept_id = (select concept_id from concept where uuid = '6557646c-977f-11e1-8993-905e29aff6c1');
 
 update obs b
-set b.value_coded = @no_concept_id
+set b.value_coded = @no_concept_id, b.value_numeric=null
 where b.obs_id in ( select * from (
   select o.obs_id from obs o where o.concept_id=@cpt_concept_id and o.value_coded is null and o.value_numeric=0 and o.voided=0
 ) tblTmp);
 
 
 update obs b
-set b.value_coded = @yes_concept_id
+set b.value_coded = @yes_concept_id, b.value_numeric=null
 where b.obs_id in ( select * from (
   select o.obs_id from obs o where o.concept_id=@cpt_concept_id and o.value_coded is null and o.value_numeric=1 and o.voided=0
 ) tblTmp);
