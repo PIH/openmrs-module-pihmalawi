22682fa116122b8def142e43ca0a725c532c0b9b
Michael Seaton <mseaton@pih.org>
2026-03-02 14:37:48 -0500

    MLW-1792 - Fix invalid concepts

--- api/src/main/resources/pihmalawiMigrations/fixInvalidConceptsMarch2026.sql
+++ api/src/main/resources/pihmalawiMigrations/fixInvalidConceptsMarch2026.sql
@@ -66,3 +66,72 @@ where sn.concept_name_type = 'SHORT'
 
 # There are a few dozen concepts that have names that match other concepts.  These need to be dealt with one by one
 
+# Following BB
+update concept set retired = 1, retired_by = 1, date_retired = now(), retire_reason = 'Duplicate of 6561ab7a-977f-11e1-8993-905e29aff6c1' where uuid = '65631ef6-977f-11e1-8993-905e29aff6c1';
+
+# Aspirin
+update concept set retired = 1, retired_by = 1, date_retired = now(), retire_reason = 'Duplicate of 6545efde-977f-11e1-8993-905e29aff6c1' where uuid = '656360f0-977f-11e1-8993-905e29aff6c1';
+
+# Community volunteer
+update concept set retired = 1, retired_by = 1, date_retired = now(), retire_reason = 'Duplicate of 6566ef36-977f-11e1-8993-905e29aff6c1' where uuid = '657d1cca-977f-11e1-8993-905e29aff6c1';
+
+# Decreased sensation
+update concept set retired = 1, retired_by = 1, date_retired = now(), retire_reason = 'Duplicate of 6569f5aa-977f-11e1-8993-905e29aff6c1' where uuid = '654886a4-977f-11e1-8993-905e29aff6c1';
+
+# Lymphocyte count datetime
+update concept set retired = 1, retired_by = 1, date_retired = now(), retire_reason = 'Duplicate of 657492da-977f-11e1-8993-905e29aff6c1' where uuid = '65777a4a-977f-11e1-8993-905e29aff6c1';
+
+# Second line tuberculosis drugs
+update concept_name n inner join concept c on n.concept_id = c.concept_id
+set n.name = 'Has the patient used second-line tuberculosis drugs'
+where n.name = 'Second line tuberculosis drugs' and c.uuid = '65605f86-977f-11e1-8993-905e29aff6c1';
+
+# Extremity exam findings
+update concept set retired = 1, retired_by = 1, date_retired = now(), retire_reason = 'Duplicate of 6557aea4-977f-11e1-8993-905e29aff6c1' where uuid = '65680efc-977f-11e1-8993-905e29aff6c1';
+
+# Orphan
+update concept set retired = 1, retired_by = 1, date_retired = now(), retire_reason = 'Duplicate of 656fc57a-977f-11e1-8993-905e29aff6c1' where uuid = '65583cb6-977f-11e1-8993-905e29aff6c1';
+
+# Pulmonary tuberculosis
+update concept_name n inner join concept c on n.concept_id = c.concept_id
+set n.name = 'Does the patient have pulmonary tuberculosis'
+where n.name = 'Pulmonary tuberculosis' and c.uuid = '655a4bbe-977f-11e1-8993-905e29aff6c1';
+
+# Patient hospitalized
+update concept_name n inner join concept c on n.concept_id = c.concept_id
+set n.name = 'Patient hospitalized location'
+where n.name = 'Patient hospitalized' and c.uuid = '6558bb6e-977f-11e1-8993-905e29aff6c1';
+
+# Tinea cruris
+select concept_id into @tineaToRetire from concept where uuid = '6574208e-977f-11e1-8993-905e29aff6c1';
+select concept_id into @tineaToKeep from concept where uuid = '6567c5be-977f-11e1-8993-905e29aff6c1';
+update obs set value_coded = @tineaToKeep where value_coded = @tineaToRetire and voided = 0 and @tineaToRetire is not null and @tineaToKeep is not null;
+update concept set retired = 1, retired_by = 1, date_retired = now(), retire_reason = 'Duplicate of 6567c5be-977f-11e1-8993-905e29aff6c1' where concept_id = @tineaToRetire;
+
+# Aortic regurgitation
+update concept set retired = 1, retired_by = 1, date_retired = now(), retire_reason = 'Duplicate of 65632ce8-977f-11e1-8993-905e29aff6c1' where uuid = '6568d21a-977f-11e1-8993-905e29aff6c1';
+
+# Carotid findings
+update concept set retired = 1, retired_by = 1, date_retired = now(), retire_reason = 'Duplicate of 6568c176-977f-11e1-8993-905e29aff6c1' where uuid = '656fe578-977f-11e1-8993-905e29aff6c1';
+
+# Sulfadoxine and Pyrimethamine
+update concept set retired = 1, retired_by = 1, date_retired = now(), retire_reason = 'Duplicate of 6545eed0-977f-11e1-8993-905e29aff6c1' where uuid = '654b0b72-977f-11e1-8993-905e29aff6c1';
+
+# Postpartum hemorrhage
+update concept set retired = 1, retired_by = 1, date_retired = now(), retire_reason = 'Duplicate of 6545c7d4-977f-11e1-8993-905e29aff6c1' where uuid = '6546ea42-977f-11e1-8993-905e29aff6c1';
+
+# Urinalysis
+update concept set retired = 1, retired_by = 1, date_retired = now(), retire_reason = 'Duplicate of 6547572a-977f-11e1-8993-905e29aff6c1' where uuid = '655a447a-977f-11e1-8993-905e29aff6c1';
+
+# Erythrocyte sedimentation rate
+update concept set retired = 1, retired_by = 1, date_retired = now(), retire_reason = 'Duplicate of 654a754a-977f-11e1-8993-905e29aff6c1' where uuid = '65659f28-977f-11e1-8993-905e29aff6c1';
+
+# People with HIV avoid friendships rather than worry about telling people about their HIV
+update concept_name n inner join concept c on n.concept_id = c.concept_id
+set n.name = 'Community Stigma 14 - People with HIV avoid friendships rather than worry about telling people about their HIV'
+where n.name = 'People with HIV avoid friendships rather than worry about telling people about their HIV' and c.uuid = '655c7150-977f-11e1-8993-905e29aff6c1';
+
+# Parotitis
+update concept_name n inner join concept c on n.concept_id = c.concept_id
+set n.name = 'Parotitis (finding)'
+where n.name = 'Parotitis' and c.uuid = '65696040-977f-11e1-8993-905e29aff6c1';
