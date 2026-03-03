5ab290fd77aeff2151773aa7381a388b16be5e1e
Cosmin <cioan@pih.org>
2026-03-03 15:35:36 -0500

    MLW-1792: remove database name from the SQL script

--- api/src/main/resources/pihmalawiMigrations/retireDefaultTrueFalseConcepts.sql
+++ api/src/main/resources/pihmalawiMigrations/retireDefaultTrueFalseConcepts.sql
@@ -6,5 +6,5 @@ select concept_id into @defaultTrueConcept from concept where uuid = 'cf82933b-3
 select concept_id into @defaultFalseConcept from concept where uuid = '488b58ff-64f5-4f8a-8979-fa79940b1594';
 
 delete from concept_name where concept_id in (@defaultTrueConcept, @defaultFalseConcept) and locale != 'en' and concept_name_type != 'FULLY_SPECIFIED';
-update concept_name set name = concat(name, ' Default') where neno.concept_name.concept_id in (@defaultTrueConcept, @defaultFalseConcept);
+update concept_name set name = concat(name, ' Default') where concept_name.concept_id in (@defaultTrueConcept, @defaultFalseConcept);
 update concept set retired = 1, retired_by = 1, date_retired = now(), retire_reason = 'retiring default true and false concepts' where concept_id in (@defaultTrueConcept, @defaultFalseConcept);
